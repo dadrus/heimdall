ccc68b3252f19147d5f2694810766f6c7fd47b48
Dimitrij Drus <dadrus@gmx.de>
2022-03-25 20:41:00 +0100

    rules fx module extended to make use of provider modules

--- internal/rules/rulesfx.go
+++ internal/rules/rulesfx.go
@@ -1,7 +1,15 @@
 package rules
 
-import "go.uber.org/fx"
+import (
+	"go.uber.org/fx"
+
+	"github.com/dadrus/heimdall/internal/rules/provider"
+)
 
 var Module = fx.Options(
 	fx.Provide(NewRepository),
+	fx.Provide(func() provider.RuleSetChangedEventQueue {
+		return make(provider.RuleSetChangedEventQueue, 20)
+	}),
+	provider.Module,
 )
