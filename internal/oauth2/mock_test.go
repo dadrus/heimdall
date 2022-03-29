f6a735ead75daeef10b5f7215cc3e31d3d23acb7
Dimitrij Drus <dadrus@gmx.de>
2022-03-29 20:04:06 +0200

    jwt payload tests

---
+++ internal/oauth2/mock_test.go
@@ -0,0 +1,27 @@
+package oauth2
+
+import "github.com/stretchr/testify/mock"
+
+type MockClaimAsserter struct {
+	mock.Mock
+}
+
+func (a *MockClaimAsserter) AssertIssuer(issuer string) error {
+	args := a.Called(issuer)
+	return args.Error(0)
+}
+
+func (a *MockClaimAsserter) AssertAudience(audience []string) error {
+	args := a.Called(audience)
+	return args.Error(0)
+}
+
+func (a *MockClaimAsserter) AssertScopes(scopes []string) error {
+	args := a.Called(scopes)
+	return args.Error(0)
+}
+
+func (a *MockClaimAsserter) AssertValidity(nbf, exp int64) error {
+	args := a.Called(nbf, exp)
+	return args.Error(0)
+}
