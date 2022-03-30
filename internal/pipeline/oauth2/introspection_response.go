566aad4503ee422c7a1346c88163f4c192defb44
Dimitrij Drus <dadrus@gmx.de>
2022-03-30 08:30:29 +0200

    type definition changed

--- internal/pipeline/oauth2/introspection_response.go
+++ internal/pipeline/oauth2/introspection_response.go
@@ -2,22 +2,46 @@ package oauth2
 
 import (
 	"errors"
-
-	"gopkg.in/square/go-jose.v2/jwt"
+	"strings"
 )
 
-type IntrospectionResponse struct {
-	jwt.Claims
-	Active    bool   `json:"active"`
-	Scopes    Scopes `json:"scope"`
-	ClientId  string `json:"client_id"`
-	Username  string `json:"username"`
-	TokenType string `json:"token_type"`
+type IntrospectionResponse map[string]interface{}
+
+// Active    bool   `json:"active"`
+// Scopes    Scopes `json:"scope"`
+// ClientId  string `json:"client_id"`
+// Username  string `json:"username"`
+// TokenType string `json:"token_type"`
+
+func (ir IntrospectionResponse) checkScopes(asserter ClaimAsserter) error {
+	var scopes []string
+	scope := getClaim(ir, "scope", "")
+	if len(scope) == 0 {
+		scopes = getClaim(ir, "scope", []string{})
+	} else {
+		scopes = strings.Split(scope, " ")
+	}
+
+	if len(scopes) == 0 {
+		scp := getClaim(ir, "scp", "")
+		if len(scp) == 0 {
+			scopes = getClaim(ir, "scp", []string{})
+		} else {
+			scopes = strings.Split(scp, " ")
+		}
+	}
+
+	return asserter.AssertScopes(scopes)
 }
 
-func (ir *IntrospectionResponse) Verify(asserter ClaimAsserter) error {
-	if !ir.Active {
+func (ir IntrospectionResponse) Verify(asserter ClaimAsserter) error {
+	active := getClaim(ir, "active", false)
+	if !active {
 		return errors.New("token is not active")
+	}
+
+	if err := ir.checkScopes(asserter); err != nil {
+		return err
 	}
 
 	return nil
