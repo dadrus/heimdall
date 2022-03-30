b29228e4c9dbffd06a4e31f2f19e4373cced28c4
Dimitrij Drus <dadrus@gmx.de>
2022-03-30 19:33:46 +0200

    more config & checks

--- internal/pipeline/handler/authenticators/oauth2_introspection_authenticator.go
+++ internal/pipeline/handler/authenticators/oauth2_introspection_authenticator.go
@@ -13,13 +13,14 @@ import (
 	"github.com/dadrus/heimdall/internal/pipeline/handler"
 	"github.com/dadrus/heimdall/internal/pipeline/handler/authenticators/extractors"
 	"github.com/dadrus/heimdall/internal/pipeline/oauth2"
+	"gopkg.in/square/go-jose.v2"
 )
 
 type oauth2IntrospectionAuthenticator struct {
-	AuthDataGetter   AuthDataGetter
-	Endpoint         Endpoint
-	SubjectExtractor SubjectExtrator
-	Assertions       oauth2.Expectation
+	e   Endpoint
+	a   oauth2.Expectation
+	se  SubjectExtrator
+	adg AuthDataGetter
 }
 
 func NewOAuth2IntrospectionAuthenticatorFromJSON(rawConfig json.RawMessage) (*oauth2IntrospectionAuthenticator, error) {
@@ -34,8 +35,35 @@ func NewOAuth2IntrospectionAuthenticatorFromJSON(rawConfig json.RawMessage) (*oa
 		return nil, err
 	}
 
-	c.Endpoint.Headers["Content-Type"] = "application/x-www-form-urlencoded"
-	c.Endpoint.Headers["Accept-Type"] = "application/json"
+	if len(c.Assertions.AllowedAlgorithms) == 0 {
+		c.Assertions.AllowedAlgorithms = []string{
+			// ECDSA
+			string(jose.ES256), string(jose.ES384), string(jose.ES512),
+			// RSA-PSS
+			string(jose.PS256), string(jose.PS384), string(jose.PS512),
+		}
+	}
+
+	if err := c.Assertions.Validate(); err != nil {
+		return nil, &errorsx.ArgumentError{
+			Message: "failed to validate assertions configuration",
+			Cause:   err,
+		}
+	}
+
+	if c.Endpoint.Headers == nil {
+		c.Endpoint.Headers = make(map[string]string)
+	}
+
+	if _, ok := c.Endpoint.Headers["Content-Type"]; !ok {
+		c.Endpoint.Headers["Content-Type"] = "application/x-www-form-urlencoded"
+	}
+	if _, ok := c.Endpoint.Headers["Accept-Type"]; !ok {
+		c.Endpoint.Headers["Accept-Type"] = "application/json"
+	}
+	if len(c.Endpoint.Method) == 0 {
+		c.Endpoint.Method = "POST"
+	}
 
 	extractor := extractors.CompositeExtractStrategy{
 		extractors.HeaderValueExtractStrategy{Name: "Authorization", Prefix: "Bearer"},
@@ -44,25 +72,25 @@ func NewOAuth2IntrospectionAuthenticatorFromJSON(rawConfig json.RawMessage) (*oa
 	}
 
 	return &oauth2IntrospectionAuthenticator{
-		AuthDataGetter:   extractor,
-		Endpoint:         c.Endpoint,
-		Assertions:       c.Assertions,
-		SubjectExtractor: &c.Session,
+		adg: extractor,
+		e:   c.Endpoint,
+		a:   c.Assertions,
+		se:  &c.Session,
 	}, nil
 }
 
 func (a *oauth2IntrospectionAuthenticator) Authenticate(ctx context.Context, as handler.RequestContext, sc *heimdall.SubjectContext) error {
-	accessToken, err := a.AuthDataGetter.GetAuthData(as)
+	accessToken, err := a.adg.GetAuthData(as)
 	if err != nil {
 		return &errorsx.ArgumentError{Message: "no access token present", Cause: err}
 	}
 
-	data := url.Values{
-		"token":           []string{accessToken},
-		"token_type_hint": []string{"access_token"},
-	}
-
-	rawBody, err := a.Endpoint.SendRequest(ctx, strings.NewReader(data.Encode()))
+	rawBody, err := a.e.SendRequest(ctx, strings.NewReader(
+		url.Values{
+			"token":           []string{accessToken},
+			"token_type_hint": []string{"access_token"},
+		}.Encode()),
+	)
 	if err != nil {
 		return err
 	}
@@ -72,14 +100,14 @@ func (a *oauth2IntrospectionAuthenticator) Authenticate(ctx context.Context, as 
 		return fmt.Errorf("failed to unmarshal introspection response: %w", err)
 	}
 
-	if err = resp.Validate(a.Assertions); err != nil {
+	if err = resp.Validate(a.a); err != nil {
 		return &errorsx.UnauthorizedError{
 			Message: "access token does not satisfy assertion conditions",
 			Cause:   err,
 		}
 	}
 
-	if sc.Subject, err = a.SubjectExtractor.GetSubject(rawBody); err != nil {
+	if sc.Subject, err = a.se.GetSubject(rawBody); err != nil {
 		return fmt.Errorf("failed to extract subject information: %w", err)
 	}
 
@@ -102,9 +130,9 @@ func (a *oauth2IntrospectionAuthenticator) WithConfig(config []byte) (handler.Au
 	}
 
 	return &oauth2IntrospectionAuthenticator{
-		Endpoint:         a.Endpoint,
-		Assertions:       c.Assertions,
-		SubjectExtractor: a.SubjectExtractor,
-		AuthDataGetter:   a.AuthDataGetter,
+		e:   a.e,
+		a:   c.Assertions,
+		se:  a.se,
+		adg: a.adg,
 	}, nil
 }
