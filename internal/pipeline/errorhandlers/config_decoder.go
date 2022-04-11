d195671253bb0c3da22ed007ecd902f9ed1da019
Dimitrij Drus <dadrus@gmx.de>
2022-04-11 12:28:09 +0200

    mapstructure decoder for error handlers

---
+++ internal/pipeline/errorhandlers/config_decoder.go
@@ -0,0 +1,23 @@
+package errorhandlers
+
+import (
+	"github.com/mitchellh/mapstructure"
+)
+
+func decodeConfig(input any, output any) error {
+	dec, err := mapstructure.NewDecoder(
+		&mapstructure.DecoderConfig{
+			DecodeHook: mapstructure.ComposeDecodeHookFunc(
+				mapstructure.StringToTimeDurationHookFunc(),
+				DecodeCIDRMatcherHookFunc(),
+				DecodeErrorTypeMatcherHookFunc(),
+			),
+			Result:      output,
+			ErrorUnused: true,
+		})
+	if err != nil {
+		return err
+	}
+
+	return dec.Decode(input)
+}
