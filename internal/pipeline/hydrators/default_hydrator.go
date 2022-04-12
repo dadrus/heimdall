84738ede27d60ec4d179ddb9fa63bcb3588b0243
Dimitrij Drus <dadrus@gmx.de>
2022-04-12 10:49:25 +0200

    imports organized

--- internal/pipeline/hydrators/default_hydrator.go
+++ internal/pipeline/hydrators/default_hydrator.go
@@ -2,7 +2,7 @@ package hydrators
 
 import (
 	"github.com/rs/zerolog"
-	
+
 	"github.com/dadrus/heimdall/internal/config"
 	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/pipeline/subject"
