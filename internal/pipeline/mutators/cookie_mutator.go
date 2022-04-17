015d7ef02c433c6dacd3f7b0f0e32fc470f0c8f0
Dimitrij Drus <dadrus@gmx.de>
2022-04-17 22:10:42 +0200

    checking if subject is not nil

--- internal/pipeline/mutators/cookie_mutator.go
+++ internal/pipeline/mutators/cookie_mutator.go
@@ -49,6 +49,11 @@ func (m *cookieMutator) Execute(ctx heimdall.Context, sub *subject.Subject) erro
 	logger := zerolog.Ctx(ctx.AppContext())
 	logger.Debug().Msg("Mutating using cookie mutator")
 
+	if sub == nil {
+		return errorchain.NewWithMessage(heimdall.ErrArgument,
+			"failed to execute cookie mutator due to 'nil' subject")
+	}
+
 	for name, tmpl := range m.cookies {
 		value, err := tmpl.Render(sub)
 		if err != nil {
