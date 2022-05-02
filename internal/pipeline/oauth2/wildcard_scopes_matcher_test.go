7aab1f831c9b3f096cae76a39f0c9c0f1a6abf77
Dimitrij Drus <dadrus@gmx.de>
2022-05-02 17:12:31 +0200

    test refactored

--- internal/pipeline/oauth2/wildcard_scopes_matcher_test.go
+++ internal/pipeline/oauth2/wildcard_scopes_matcher_test.go
@@ -1,7 +1,6 @@
 package oauth2
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,72 +9,209 @@ import (
 func TestWildcardScopeStrategy(t *testing.T) {
 	t.Parallel()
 
-	strategy := WildcardScopeStrategyMatcher{}
-	scopes := []string{}
+	for _, tc := range []struct {
+		uc             string
+		requiredScopes WildcardScopeStrategyMatcher
+		providedScopes []string
+		assert         func(t *testing.T, err error)
+	}{
+		{
+			uc:             "empty required scopes match empty provides scopes",
+			requiredScopes: WildcardScopeStrategyMatcher{},
+			providedScopes: []string{},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "empty string required scope doesn't match wildcard",
+			requiredScopes: WildcardScopeStrategyMatcher{""},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"*"}
-	assert.False(t, strategy.doMatch(scopes, ""))
-	assert.True(t, strategy.doMatch(scopes, "asdf"))
-	assert.True(t, strategy.doMatch(scopes, "asdf.asdf"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "empty string required scope doesn't match provided specific clam",
+			requiredScopes: WildcardScopeStrategyMatcher{""},
+			providedScopes: []string{"foo"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo"}
-	assert.False(t, strategy.doMatch(scopes, "*"))
-	assert.False(t, strategy.doMatch(scopes, "foo.*"))
-	assert.False(t, strategy.doMatch(scopes, "fo*"))
-	assert.True(t, strategy.doMatch(scopes, "foo"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "wildcard required scope matches wildcard provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"*"},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo*"}
-	assert.False(t, strategy.doMatch(scopes, "foo"))
-	assert.False(t, strategy.doMatch(scopes, "fooa"))
-	assert.False(t, strategy.doMatch(scopes, "fo"))
-	assert.True(t, strategy.doMatch(scopes, "foo*"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "wildcard required scope doesn't match specific provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"*"},
+			providedScopes: []string{"foo"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.*"}
-	assert.True(t, strategy.doMatch(scopes, "foo.bar"))
-	assert.True(t, strategy.doMatch(scopes, "foo.baz"))
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "wildcard required scope doesn't match specific hierarchic provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"*"},
+			providedScopes: []string{"foo.bar"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.*.baz"}
-	assert.True(t, strategy.doMatch(scopes, "foo.*.baz"))
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo..baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.bar"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "wildcard required scope doesn't match specific hierarchic wildcard scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"*"},
+			providedScopes: []string{"foo.*.bar"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.*.bar.*"}
-	assert.True(t, strategy.doMatch(scopes, "foo.baz.bar.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.baz.baz.bar.baz"))
-	assert.True(t, strategy.doMatch(scopes, "foo.baz.bar.bar.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo.baz.bar"))
-	assert.True(t, strategy.doMatch(scopes, "foo.*.bar.*.*.*"))
-	assert.True(t, strategy.doMatch(scopes, "foo.1.bar.1.2.3.4.5"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "simple required scope matches wildcard as provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo"},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.*.bar"}
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.bar.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo..bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar..bar"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "simple required scope doesn't matches prefixed wildcard scope as provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo"},
+			providedScopes: []string{"foo*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.*.bar.*.baz.*"}
-	assert.False(t, strategy.doMatch(scopes, "foo.*.*"))
-	assert.False(t, strategy.doMatch(scopes, "foo.*.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo.baz.*"))
-	assert.False(t, strategy.doMatch(scopes, "foo.baz.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo.b*.bar"))
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.bar.baz.baz.baz"))
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.bar.baz.baz.baz.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.bar.baz.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.baz.baz.baz.bar"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "simple required scope doesn't matches hierarchical wildcard scope as provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo"},
+			providedScopes: []string{"foo.*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = strings.Fields("hydra.* openid offline  hydra")
-	assert.True(t, strategy.doMatch(scopes, "hydra.clients"))
-	assert.True(t, strategy.doMatch(scopes, "hydra.clients.get"))
-	assert.True(t, strategy.doMatch(scopes, "hydra"))
-	assert.True(t, strategy.doMatch(scopes, "offline"))
-	assert.True(t, strategy.doMatch(scopes, "openid"))
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "single simple hierarchical required scope matches wildcard provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "single simple required scope does not match empty provided scopes",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "single simple hierarchical required scope matches root dor-prefixed wildcard provided scopes",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"foo.*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "single simple hierarchical required scope doesn't match root prefixed wildcard provided scopes",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"foo*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "single simple hierarchical required scope doesn't match partially root prefixed wildcard provided scopes",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"fo*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "hierarchical wildcard required scope matches wildcard provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.*.bar"},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "hierarchical wildcard required scope matches root prefixed partial wildcard provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.*.bar"},
+			providedScopes: []string{"foo.*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "hierarchical wildcard required scope doesn't match hierarchical provided scope with not matching parts",
+			requiredScopes: WildcardScopeStrategyMatcher{"foo.*.bar"},
+			providedScopes: []string{"foo.baz.bar"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "partially wildcard required scope matches wildcard provided scope",
+			requiredScopes: WildcardScopeStrategyMatcher{"fo*"},
+			providedScopes: []string{"*"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// WHEN
+			err := tc.requiredScopes.Match(tc.providedScopes)
+
+			// THEN
+			tc.assert(t, err)
+		})
+	}
 }
