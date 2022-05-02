8be4f43391af68800c114a86c2b22bf6bce396f7
Dimitrij Drus <dadrus@gmx.de>
2022-05-02 15:08:37 +0200

    test refactored

--- internal/pipeline/oauth2/hierarchic_scopes_matcher_test.go
+++ internal/pipeline/oauth2/hierarchic_scopes_matcher_test.go
@@ -9,37 +9,101 @@ import (
 func TestHierarchicScopeStrategy(t *testing.T) {
 	t.Parallel()
 
-	strategy := HierarchicScopeStrategyMatcher{}
-	scopes := []string{}
+	for _, tc := range []struct {
+		uc             string
+		requiredScopes HierarchicScopeStrategyMatcher
+		providedScopes []string
+		assert         func(t *testing.T, err error)
+	}{
+		{
+			uc:             "empty required scopes match single provided scope",
+			requiredScopes: HierarchicScopeStrategyMatcher{},
+			providedScopes: []string{"foo.bar"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	assert.False(t, strategy.doMatch(scopes, "foo.bar.baz"))
-	assert.False(t, strategy.doMatch(scopes, "foo.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "empty required scopes match multiple provided scopes",
+			requiredScopes: HierarchicScopeStrategyMatcher{},
+			providedScopes: []string{"foo.bar", "bar.foo"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"foo.bar", "bar.baz", "baz.baz.1", "baz.baz.2", "baz.baz.3", "baz.baz.baz"}
-	assert.True(t, strategy.doMatch(scopes, "foo.bar.baz"))
-	assert.True(t, strategy.doMatch(scopes, "baz.baz.baz"))
-	assert.True(t, strategy.doMatch(scopes, "foo.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "empty required scopes do match empty provided scopes",
+			requiredScopes: HierarchicScopeStrategyMatcher{},
+			providedScopes: []string{"foo.bar", "bar.foo"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	assert.True(t, strategy.doMatch(scopes, "bar.baz"))
-	assert.True(t, strategy.doMatch(scopes, "bar.baz.zad"))
-	assert.False(t, strategy.doMatch(scopes, "bar"))
-	assert.False(t, strategy.doMatch(scopes, "baz"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "required scopes with single element match provided scopes with exact same single scope",
+			requiredScopes: HierarchicScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"foo.bar"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"fosite.keys.create", "fosite.keys.get", "fosite.keys.delete", "fosite.keys.update"}
-	assert.True(t, strategy.doMatch(scopes, "fosite.keys.delete"))
-	assert.True(t, strategy.doMatch(scopes, "fosite.keys.get"))
-	assert.True(t, strategy.doMatch(scopes, "fosite.keys.get"))
-	assert.True(t, strategy.doMatch(scopes, "fosite.keys.update"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			uc:             "required scopes with single element matches provided scopes with a single scope on root level",
+			requiredScopes: HierarchicScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"foo"}, // foo includes foo.bar, foo.baz, foo.*, etc
+			assert: func(t *testing.T, err error) {
+				t.Helper()
 
-	scopes = []string{"hydra", "openid", "offline"}
-	assert.False(t, strategy.doMatch(scopes, "foo.bar"))
-	assert.False(t, strategy.doMatch(scopes, "foo"))
-	assert.True(t, strategy.doMatch(scopes, "hydra"))
-	assert.True(t, strategy.doMatch(scopes, "hydra.bar"))
-	assert.True(t, strategy.doMatch(scopes, "openid"))
-	assert.True(t, strategy.doMatch(scopes, "openid.baz.bar"))
-	assert.True(t, strategy.doMatch(scopes, "offline"))
-	assert.True(t, strategy.doMatch(scopes, "offline.baz.bar.baz"))
+				assert.NoError(t, err)
+			},
+		},
+		{
+			// nolint: lll
+			uc:             "required scopes with single element doesn't match provided scopes with a single scope having required scope as prefix",
+			requiredScopes: HierarchicScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{"foo.bar.baz"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			uc:             "required scopes with single element doesn't match empty provided scopes",
+			requiredScopes: HierarchicScopeStrategyMatcher{"foo.bar"},
+			providedScopes: []string{},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.Error(t, err)
+			},
+		},
+		{
+			// nolint: lll
+			uc:             "required scopes with multiple elements match provided scopes in hierarchy, which also include further scopes",
+			requiredScopes: HierarchicScopeStrategyMatcher{"foo.bar", "bar.foo"},
+			providedScopes: []string{"foo", "bar.foo", "baz"},
+			assert: func(t *testing.T, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// WHEN
+			err := tc.requiredScopes.Match(tc.providedScopes)
+
+			// THEN
+			tc.assert(t, err)
+		})
+	}
 }
