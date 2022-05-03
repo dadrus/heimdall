512423580ad0ad28c06aa54f8bc03160c5414954
Dimitrij Drus <dadrus@gmx.de>
2022-05-03 10:16:37 +0200

    config decoder for authorizers, which require configuration

---
+++ internal/pipeline/authorizers/config_decoder.go
@@ -0,0 +1,21 @@
+package authorizers
+
+import (
+	"github.com/mitchellh/mapstructure"
+)
+
+func decodeConfig(input any, output any) error {
+	dec, err := mapstructure.NewDecoder(
+		&mapstructure.DecoderConfig{
+			DecodeHook: mapstructure.ComposeDecodeHookFunc(
+				mapstructure.StringToTimeDurationHookFunc(),
+			),
+			Result:      output,
+			ErrorUnused: true,
+		})
+	if err != nil {
+		return err
+	}
+
+	return dec.Decode(input)
+}
