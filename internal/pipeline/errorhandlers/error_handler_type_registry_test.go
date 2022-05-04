7c1500355a8d1d963bac80d59bd17379f5a4e753
Dimitrij Drus <dadrus@gmx.de>
2022-05-04 16:33:56 +0200

    new tests

---
+++ internal/pipeline/errorhandlers/error_handler_type_registry_test.go
@@ -0,0 +1,52 @@
+package errorhandlers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/config"
+)
+
+func TestCreateErrorHandlerPrototypePrototype(t *testing.T) {
+	t.Parallel()
+
+	// there are 3 error handlers implemented, which should have been registered
+	require.Len(t, errorHandlerTypeFactories, 3)
+
+	for _, tc := range []struct {
+		uc     string
+		typ    config.PipelineObjectType
+		assert func(t *testing.T, err error, errorHandler ErrorHandler)
+	}{
+		{
+			uc:  "using known type",
+			typ: config.POTDefault,
+			assert: func(t *testing.T, err error, errorHandler ErrorHandler) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.IsType(t, &defaultErrorHandler{}, errorHandler)
+			},
+		},
+		{
+			uc:  "using unknown type",
+			typ: config.POTDeny,
+			assert: func(t *testing.T, err error, errorHandler ErrorHandler) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.ErrorIs(t, err, ErrUnsupportedErrorHandlerType)
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// WHEN
+			errorHandler, err := CreateErrorHandlerPrototype("foo", tc.typ, nil)
+
+			// THEN
+			tc.assert(t, err, errorHandler)
+		})
+	}
+}
