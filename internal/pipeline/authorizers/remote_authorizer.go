2c0e3bf6521e6f9129d336a39ff72768e0ad57a4
Dimitrij Drus <dadrus@gmx.de>
2022-05-07 15:56:58 +0200

    cache support added

--- internal/pipeline/authorizers/remote_authorizer.go
+++ internal/pipeline/authorizers/remote_authorizer.go
@@ -1,18 +1,18 @@
 package authorizers
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/rs/zerolog"
 
+	"github.com/dadrus/heimdall/internal/cache"
 	"github.com/dadrus/heimdall/internal/config"
 	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/pipeline/endpoint"
@@ -42,6 +42,12 @@ type remoteAuthorizer struct {
 	headers            map[string]template.Template
 	payload            template.Template
 	headersForUpstream []string
+	ttl                *time.Duration
+}
+
+type authorizationInformation struct {
+	header  http.Header
+	payload map[string]any
 }
 
 func newRemoteAuthorizer(name string, rawConfig map[any]any) (*remoteAuthorizer, error) {
@@ -50,6 +56,7 @@ func newRemoteAuthorizer(name string, rawConfig map[any]any) (*remoteAuthorizer,
 		Headers                  map[string]template.Template `mapstructure:"headers"`
 		Payload                  template.Template            `mapstructure:"payload"`
 		ResponseHeadersToForward []string                     `mapstructure:"forward_response_headers_to_upstream"`
+		CacheTTL                 *time.Duration               `mapstructure:"cache_ttl"`
 	}
 
 	var conf _config
@@ -81,6 +88,7 @@ func newRemoteAuthorizer(name string, rawConfig map[any]any) (*remoteAuthorizer,
 		payload:            conf.Payload,
 		headers:            conf.Headers,
 		headersForUpstream: conf.ResponseHeadersToForward,
+		ttl:                conf.CacheTTL,
 	}, nil
 }
 
@@ -88,51 +96,91 @@ func (a *remoteAuthorizer) Execute(ctx heimdall.Context, sub *subject.Subject) e
 	logger := zerolog.Ctx(ctx.AppContext())
 	logger.Debug().Msg("Authorizing using remote authorizer")
 
+	cch := cache.Ctx(ctx.AppContext())
+
+	var (
+		cacheKey string
+		authInfo *authorizationInformation
+		err      error
+	)
+
+	if a.ttl != nil {
+		cacheKey = a.calculateCacheKey(sub)
+		if item := cch.Get(cacheKey); item != nil {
+			if cachedResponse, ok := item.(*authorizationInformation); !ok {
+				logger.Warn().Msg("Wrong object type from cache")
+				cch.Delete(cacheKey)
+			} else {
+				logger.Debug().Msg("Reusing introspection response from cache")
+
+				authInfo = cachedResponse
+			}
+		}
+	}
+
+	if authInfo == nil {
+		authInfo, err = a.doAuthorize(ctx, sub)
+		if err != nil {
+			return err
+		}
+
+		if a.ttl != nil {
+			cch.Set(cacheKey, authInfo, *a.ttl)
+		}
+	}
+
+	for _, headerName := range a.headersForUpstream {
+		headerValue := authInfo.header.Get(headerName)
+		if len(headerValue) != 0 {
+			ctx.AddResponseHeader(headerName, headerValue)
+		}
+	}
+
+	if len(authInfo.payload) != 0 {
+		sub.Attributes[a.name] = authInfo.payload
+	}
+
+	return nil
+}
+
+func (a *remoteAuthorizer) doAuthorize(ctx heimdall.Context, sub *subject.Subject) (*authorizationInformation, error) {
 	req, err := a.createRequest(ctx, sub)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resp, err := a.e.CreateClient(req.URL.Hostname()).Do(req)
 	if err != nil {
 		var clientErr *url.Error
 		if errors.As(err, &clientErr) && clientErr.Timeout() {
-			return errorchain.NewWithMessage(heimdall.ErrCommunicationTimeout,
+			return nil, errorchain.NewWithMessage(heimdall.ErrCommunicationTimeout,
 				"request to the authorization endpoint timed out").CausedBy(err)
 		}
 
-		return errorchain.NewWithMessage(heimdall.ErrCommunication,
+		return nil, errorchain.NewWithMessage(heimdall.ErrCommunication,
 			"request to the authorization endpoint failed").CausedBy(err)
 	}
 
 	defer resp.Body.Close()
 
-	authorizerResponse, err := a.readResponse(resp)
-	if err != nil {
-		return err
-	}
-
-	for _, headerName := range a.headersForUpstream {
-		headerValue := resp.Header.Get(headerName)
-		if len(headerValue) != 0 {
-			ctx.AddResponseHeader(headerName, headerValue)
-		}
-	}
-
-	if len(authorizerResponse) != 0 {
-		sub.Attributes[a.name] = authorizerResponse
-	}
-
-	return nil
-}
-
-func (a *remoteAuthorizer) createRequest(ctx heimdall.Context, sub *subject.Subject) (*http.Request, error) {
-	payload, err := a.createRequestPayload(ctx, sub)
+	data, err := a.readResponse(resp)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := a.e.CreateRequest(ctx.AppContext(), payload)
+	return &authorizationInformation{
+		header:  resp.Header,
+		payload: data,
+	}, nil
+}
+
+func (a *remoteAuthorizer) createRequest(ctx heimdall.Context, sub *subject.Subject) (*http.Request, error) {
+	body, err := a.payload.Render(ctx, sub)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := a.e.CreateRequest(ctx.AppContext(), strings.NewReader(body))
 	if err != nil {
 		return nil, err
 	}
@@ -200,15 +248,6 @@ func (a *remoteAuthorizer) WithConfig(rawConfig map[any]any) (Authorizer, error)
 	}, nil
 }
 
-func (a *remoteAuthorizer) createRequestPayload(ctx heimdall.Context, sub *subject.Subject) (io.Reader, error) {
-	if a.payload == "original_body" {
-		return bytes.NewReader(ctx.RequestBody()), nil
-	}
-
-	body, err := a.payload.Render(ctx, sub)
-	if err != nil {
-		return nil, err
-	}
-
-	return strings.NewReader(body), nil
+func (a *remoteAuthorizer) calculateCacheKey(sub *subject.Subject) string {
+	return ""
 }
