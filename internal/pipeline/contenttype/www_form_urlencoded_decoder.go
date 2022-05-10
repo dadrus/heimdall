ad28cb4003e3adf8bc2b5237c0d07828ecd477a5
Dimitrij Drus <dadrus@gmx.de>
2022-05-10 18:34:04 +0200

    code simplified

--- internal/pipeline/contenttype/www_form_urlencoded_decoder.go
+++ internal/pipeline/contenttype/www_form_urlencoded_decoder.go
@@ -7,10 +7,5 @@ import (
 type WWWFormUrlencodedDecoder struct{}
 
 func (WWWFormUrlencodedDecoder) Decode(rawData []byte) (any, error) {
-	values, err := url.ParseQuery(string(rawData))
-	if err != nil {
-		return nil, err
-	}
-
-	return values, nil
+	return url.ParseQuery(string(rawData))
 }
