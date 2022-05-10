8c4c514e3f4b205e0125ea60e6059943be78db82
Dimitrij Drus <dadrus@gmx.de>
2022-05-10 19:00:41 +0200

    new tests

---
+++ internal/pipeline/hydrators/hydrator_type_registry_test.go
@@ -0,0 +1,53 @@
+package hydrators
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/config"
+	"github.com/dadrus/heimdall/internal/heimdall"
+)
+
+func TestCreateHydratorPrototype(t *testing.T) {
+	t.Parallel()
+
+	// there are 3 error handlers implemented, which should have been registered
+	require.Len(t, hydratorTypeFactories, 1)
+
+	for _, tc := range []struct {
+		uc     string
+		typ    config.PipelineObjectType
+		assert func(t *testing.T, err error, hydrator Hydrator)
+	}{
+		{
+			uc:  "using known type",
+			typ: config.POTGeneric,
+			assert: func(t *testing.T, err error, hydrator Hydrator) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.ErrorIs(t, err, heimdall.ErrConfiguration)
+			},
+		},
+		{
+			uc:  "using unknown type",
+			typ: config.POTDeny,
+			assert: func(t *testing.T, err error, hydrator Hydrator) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.ErrorIs(t, err, ErrUnsupportedHydratorType)
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// WHEN
+			errorHandler, err := CreateHydratorPrototype("foo", tc.typ, nil)
+
+			// THEN
+			tc.assert(t, err, errorHandler)
+		})
+	}
+}
