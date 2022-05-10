528ba1b795dbb630c2005f273e443cbd74b87666
Dimitrij Drus <dadrus@gmx.de>
2022-05-10 22:42:07 +0200

    new interface for endpoint implementation to enable rendering of context and subject aware path, query parameters, as well as header values

---
+++ internal/pipeline/renderer/renderer.go
@@ -0,0 +1,9 @@
+package renderer
+
+type Renderer interface {
+	Render(value string) (string, error)
+}
+
+type RenderFunc func(value string) (string, error)
+
+func (f RenderFunc) Render(value string) (string, error) { return f(value) }
