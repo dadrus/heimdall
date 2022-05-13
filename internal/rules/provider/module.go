86caf666401b4d5e33a8d2fdc81218b693bbf570
Dimitrij Drus <dadrus@gmx.de>
2022-05-13 23:22:48 +0200

    more logging if no rule provider is configured

--- internal/rules/provider/module.go
+++ internal/rules/provider/module.go
@@ -1,12 +1,21 @@
 package provider
 
 import (
+	"github.com/rs/zerolog"
 	"go.uber.org/fx"
 
+	"github.com/dadrus/heimdall/internal/config"
 	"github.com/dadrus/heimdall/internal/rules/provider/filesystem"
 )
 
 // nolint
 var Module = fx.Options(
+	fx.Invoke(checkRuleProvider),
 	filesystem.Module,
 )
+
+func checkRuleProvider(logger zerolog.Logger, c config.Configuration) {
+	if c.Rules.Provider.File == nil {
+		logger.Warn().Msg("No rule provider configured. Only defaults will be used.")
+	}
+}
