587292cdc38a7ee7771916db8962619b539292fe
Dimitrij Drus <dadrus@gmx.de>
2022-05-23 08:25:49 +0200

    removed cache lifecycle management methods from noop cache

--- internal/cache/noop_cache.go
+++ internal/cache/noop_cache.go
@@ -4,10 +4,6 @@ import "time"
 
 type noopCache struct{}
 
-func (c noopCache) Start() {}
-
-func (c noopCache) Stop() {}
-
 func (c noopCache) Get(_ string) any { return nil }
 
 func (c noopCache) Set(_ string, _ any, _ time.Duration) {}
