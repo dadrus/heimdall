f48f540eafcb9e3ceba439d976792b41907fd45b
Dimitrij Drus <dadrus@gmx.de>
2022-05-23 08:20:32 +0200

    cache implementaiton type renamed

--- internal/cache/memory/cache.go
+++ internal/cache/memory/cache.go
@@ -3,23 +3,22 @@ package memory
 import (
 	"time"
 
-	"github.com/dadrus/heimdall/internal/cache"
 	"github.com/jellydator/ttlcache/v3"
 )
 
-type inMemoryCache struct {
+type InMemoryCache struct {
 	c *ttlcache.Cache[string, any]
 }
 
-func New() cache.Cache {
-	return &inMemoryCache{c: ttlcache.New[string, any]()}
+func New() *InMemoryCache {
+	return &InMemoryCache{c: ttlcache.New[string, any]()}
 }
 
-func (c *inMemoryCache) Start() { c.c.Start() }
+func (c *InMemoryCache) Start() { c.c.Start() }
 
-func (c *inMemoryCache) Stop() { c.c.Stop() }
+func (c *InMemoryCache) Stop() { c.c.Stop() }
 
-func (c *inMemoryCache) Get(key string) any {
+func (c *InMemoryCache) Get(key string) any {
 	item := c.c.Get(key)
 	if item != nil && !item.IsExpired() {
 		return item.Value()
@@ -28,10 +27,10 @@ func (c *inMemoryCache) Get(key string) any {
 	return nil
 }
 
-func (c *inMemoryCache) Set(key string, value any, ttl time.Duration) {
+func (c *InMemoryCache) Set(key string, value any, ttl time.Duration) {
 	c.c.Set(key, value, ttl)
 }
 
-func (c *inMemoryCache) Delete(key string) {
+func (c *InMemoryCache) Delete(key string) {
 	c.c.Delete(key)
 }
