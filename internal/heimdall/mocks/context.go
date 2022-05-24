a896f814d551f33f1eae1d6f92a7f88cb49ff29f
Dimitrij Drus <dadrus@gmx.de>
2022-05-24 10:46:56 +0200

    code simplifications

--- internal/heimdall/mocks/context.go
+++ internal/heimdall/mocks/context.go
@@ -9,104 +9,51 @@ import (
 	"github.com/dadrus/heimdall/internal/heimdall"
 )
 
+func convertTo[T any](val any) T {
+	var def T
+
+	if val != nil {
+		// nolint: forcetypeassert
+		return val.(T)
+	}
+
+	return def
+}
+
 type MockContext struct {
 	mock.Mock
 }
 
-func (m *MockContext) RequestMethod() string {
-	return m.Called().String(0)
-}
+func (m *MockContext) RequestMethod() string { return m.Called().String(0) }
 
 func (m *MockContext) RequestHeaders() map[string]string {
-	args := m.Called()
-
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.(map[string]string)
-	}
-
-	return nil
+	return convertTo[map[string]string](m.Called().Get(0))
 }
 
-func (m *MockContext) RequestHeader(name string) string {
-	return m.Called(name).String(0)
-}
+func (m *MockContext) RequestHeader(name string) string { return m.Called(name).String(0) }
 
-func (m *MockContext) RequestCookie(name string) string {
-	return m.Called(name).String(0)
-}
+func (m *MockContext) RequestCookie(name string) string { return m.Called(name).String(0) }
 
-func (m *MockContext) RequestQueryParameter(name string) string {
-	return m.Called(name).String(0)
-}
+func (m *MockContext) RequestQueryParameter(name string) string { return m.Called(name).String(0) }
 
-func (m *MockContext) RequestFormParameter(name string) string {
-	return m.Called(name).String(0)
-}
+func (m *MockContext) RequestFormParameter(name string) string { return m.Called(name).String(0) }
 
-func (m *MockContext) RequestBody() []byte {
-	args := m.Called()
-
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.([]byte)
-	}
-
-	return nil
-}
+func (m *MockContext) RequestBody() []byte { return convertTo[[]byte](m.Called().Get(0)) }
 
 func (m *MockContext) AppContext() context.Context {
-	args := m.Called()
-
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.(context.Context)
-	}
-
-	return nil
+	return convertTo[context.Context](m.Called().Get(0))
 }
 
-func (m *MockContext) SetPipelineError(err error) {
-	m.Called(err)
-}
+func (m *MockContext) SetPipelineError(err error) { m.Called(err) }
 
-func (m *MockContext) AddResponseHeader(name, value string) {
-	m.Called(name, value)
-}
+func (m *MockContext) AddResponseHeader(name, value string) { m.Called(name, value) }
 
-func (m *MockContext) AddResponseCookie(name, value string) {
-	m.Called(name, value)
-}
+func (m *MockContext) AddResponseCookie(name, value string) { m.Called(name, value) }
 
 func (m *MockContext) Signer() heimdall.JWTSigner {
-	args := m.Called()
-
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.(heimdall.JWTSigner)
-	}
-
-	return nil
+	return convertTo[heimdall.JWTSigner](m.Called().Get(0))
 }
 
-func (m *MockContext) RequestURL() *url.URL {
-	args := m.Called()
+func (m *MockContext) RequestURL() *url.URL { return convertTo[*url.URL](m.Called().Get(0)) }
 
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.(*url.URL)
-	}
-
-	return nil
-}
-
-func (m *MockContext) RequestClientIPs() []string {
-	args := m.Called()
-
-	if i := args.Get(0); i != nil {
-		// nolint: forcetypeassert
-		return i.([]string)
-	}
-
-	return nil
-}
+func (m *MockContext) RequestClientIPs() []string { return convertTo[[]string](m.Called().Get(0)) }
