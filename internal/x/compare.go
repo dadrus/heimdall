20d872c911b3fa71f9aaf5fcfc5996e8a10a5f3f
Dimitrij Drus <dadrus@gmx.de>
2022-07-25 01:27:00 +0200

    not used function removed

--- internal/x/compare.go
+++ internal/x/compare.go
@@ -1,15 +1,5 @@
 package x
 
-func OrDefault[T comparable](val, defaultVal T) T {
-	var t T
-
-	if val == t {
-		return defaultVal
-	}
-
-	return val
-}
-
 func IfThenElse[T any](c bool, thenVal, elseVal T) T {
 	if c {
 		return thenVal
