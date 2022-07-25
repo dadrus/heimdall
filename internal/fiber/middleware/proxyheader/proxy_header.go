296693279493e404112eb3a1f9e695822e4f712c
Dimitrij Drus <dadrus@gmx.de>
2022-07-25 13:46:17 +0200

    proxyheader middleware tests added and implementation updated based on new ideas and insights from tests

--- internal/fiber/middleware/proxyheader/proxy_header.go
+++ internal/fiber/middleware/proxyheader/proxy_header.go
@@ -16,17 +16,32 @@ const (
 func New() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// reuse already present headers only, if the source is trusted
+		// otherwise delete these to avoid sending them to the upstream service
 		// these headers shall not be set by the ultimate client
-		forwardedForHeaderValue := x.IfThenElse(c.IsProxyTrusted(), c.Get(headerXForwardedFor), "")
-		forwardedHeaderValue := x.IfThenElse(c.IsProxyTrusted(), c.Get(headerForwarded), "")
+		forwardedForHeaderValue := c.Get(headerXForwardedFor)
+		if !c.IsProxyTrusted() && len(forwardedForHeaderValue) != 0 {
+			c.Request().Header.Del(headerXForwardedFor)
+
+			forwardedForHeaderValue = ""
+		}
+
+		forwardedHeaderValue := c.Get(headerForwarded)
+		if !c.IsProxyTrusted() && len(forwardedHeaderValue) != 0 {
+			c.Request().Header.Del(headerForwarded)
+
+			forwardedHeaderValue = ""
+		}
+
 		clientIP := c.IP()
 		proto := string(c.Request().URI().Scheme())
 
-		// Set either the X-Forwarded-For (if present), or the "new" Forwarded header
+		// Set the X-Forwarded-For (if present), or the "new" Forwarded header
 		if len(forwardedForHeaderValue) != 0 {
 			c.Request().Header.Set(headerXForwardedFor,
 				fmt.Sprintf("%s, %s", forwardedForHeaderValue, clientIP))
-		} else {
+		}
+
+		if len(forwardedHeaderValue) != 0 {
 			c.Request().Header.Set(headerForwarded,
 				x.IfThenElseExec(len(forwardedHeaderValue) == 0,
 					func() string { return fmt.Sprintf("for=%s;proto=%s", clientIP, proto) },
