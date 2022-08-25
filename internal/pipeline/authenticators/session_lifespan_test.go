d611a730c8b05f7816272d6704c0ab0d18623ccc
Dimitrij Drus <dadrus@gmx.de>
2022-08-25 13:28:10 +0200

    instana test dependencies removed

--- internal/pipeline/authenticators/session_lifespan_test.go
+++ internal/pipeline/authenticators/session_lifespan_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/instana/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSessionLifespanAssert(t *testing.T) {
