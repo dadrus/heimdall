fd31249c281375e98998ce994cfc123c09fbd66d
Dimitrij Drus <dadrus@gmx.de>
2022-09-02 20:06:56 +0200

    more cli docs

--- cmd/serve.go
+++ cmd/serve.go
@@ -17,9 +17,10 @@ func init() {
 	RootCmd.AddCommand(serveCmd)
 
 	serveCmd.PersistentFlags().StringP("config", "c", "",
-		"Path to heimdall's configuration file")
+		"Path to heimdall's configuration file.\n"+
+			"If not provided, the lookup sequence is:\n  1. $PWD\n  2. $HOME/.config\n  3. /etc/heimdall/")
 	serveCmd.PersistentFlags().String("env-config-prefix", "HEIMDALLCFG_",
-		"Prefix for the environment variables to consider for loading configuration from")
+		"Prefix for the environment variables to consider for\nloading configuration from")
 	serveCmd.AddCommand(serve.NewProxyCommand())
 	serveCmd.AddCommand(serve.NewDecisionCommand())
 }
