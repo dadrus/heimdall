fded8df03f003bb0b728d0c109fc2fcaf3a1f760
Dimitrij Drus <dadrus@gmx.de>
2022-09-08 02:11:54 +0200

    many different changes. Important are the configuration of index and data json files and the update to the doc search js to have proper deep linking

--- docs/themes/mytheme/assets/js/doc-search.js
+++ docs/themes/mytheme/assets/js/doc-search.js
@@ -5,7 +5,7 @@ class DocSearch extends HTMLElement {
     async connectedCallback() {
         new HugoFlexSearch({
             indexUrl: this.indexFile,
-            indexedFields: [ "title", "content", "url" ],
+            indexedFields: ["title", "content", "url"],
             limit: 10,
             suggestions: true,
             searchLogic: "and",
@@ -52,29 +52,54 @@ class DocSearch extends HTMLElement {
     resultTemplate(post) {
         const searchValue = document.getElementById("search").value
 
-        let result = `<div class="mb-2 p-1"><a href="${post.url}"><h4>${post.title}</h4></a>`
-
-        if (searchValue.length > 2) {
-            let results = []
-
-            let idx = -1
-            do {
-                idx = post.content.toLowerCase().indexOf(searchValue.toLowerCase(), idx + 1)
-                if (idx > -1) {
-                    const snippet = post.content.substring(idx - 30, idx + 30)
-                    const start = snippet.toLowerCase().indexOf(searchValue.toLowerCase())
-                    const beforeVal = snippet.substring(0, start)
-                    const afterVal = snippet.substring(start + searchValue.length)
-                    results.push('... ' + beforeVal + '<span class="text-primary">' + searchValue + '</span>' + afterVal + ' ...')
-                }
-            } while (idx > -1)
-
-            console.log(results)
-
-            result += `<p class="text-muted">`
-            results.filter((val, idx, arr) => idx < 5).forEach(res => result += `${res}<br>`)
-            result += `</p>`
+        if (searchValue.length < 3) {
+            return ""
         }
+
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(post.html_content, 'text/html')
+
+        let headingSelector = "h3"
+        let sections = Array.prototype.slice.call(doc.querySelectorAll(".sect2"))
+        let matches = sections
+            .filter(el => el.textContent.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+        if (matches.length === 0) {
+            sections = Array.prototype.slice.call(doc.querySelectorAll(".sect1"))
+            matches = sections
+                .filter(el => el.textContent.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+            headingSelector = "h2"
+        }
+
+        if (matches.length === 0) {
+            return ""
+        }
+
+        const results = matches.map(el => {
+            const section = el.querySelector(headingSelector)
+            const idx = el.textContent.toLowerCase().indexOf(searchValue.toLowerCase())
+            let snippet = el.textContent.substring(idx - 10, idx + 40)
+
+            const start = snippet.toLowerCase().indexOf(searchValue.toLowerCase())
+            const beforeVal = snippet.substring(0, start)
+            const afterVal = snippet.substring(start + searchValue.length)
+            snippet = '... ' + beforeVal + '<span class="text-primary">' + searchValue + '</span>' + afterVal + ' ...'
+
+            return {
+                "title": section.textContent,
+                "url": post.url + "#" + section.attributes.id.value,
+                "snippet": snippet
+            }
+        })
+
+        console.log(results)
+
+        let result = `<div class="mb-2 p-1"><p>${post.section} - ${post.title}</p>`
+
+        result += `<p class="text-muted">`
+        results.forEach(res => {
+            result +=  `<a href="${res.url}"><h4>${res.title}</h4></a>`
+            result += `<p class="text-muted">${res.snippet}</p>`
+        })
 
         result += `</div><hr class="mb-2" />`
 
