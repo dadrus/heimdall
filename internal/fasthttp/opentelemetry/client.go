c6f7522f59f8ee340f4416e13616cbbc6ace1e10
Dimitrij Drus <dadrus@gmx.de>
2022-09-30 18:59:02 +0200

    opentelemetry middleware for fasthhtp client

---
+++ internal/fasthttp/opentelemetry/client.go
@@ -0,0 +1,139 @@
+package opentelemetry
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/valyala/fasthttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const (
+	tracerName    = "github.com/dadrus/heimdall/internal/fasthttp/middleware/opentelemetry"
+	tracerVersion = "semver:0.1.0"
+)
+
+func newTracer(tp trace.TracerProvider) trace.Tracer {
+	return tp.Tracer(tracerName, trace.WithInstrumentationVersion(tracerVersion))
+}
+
+func NewClient(client *fasthttp.Client) *WrappedClient {
+	return &WrappedClient{client: client}
+}
+
+type WrappedClient struct {
+	client *fasthttp.Client
+}
+
+func (c *WrappedClient) DoTimeout(ctx context.Context, req *fasthttp.Request, resp *fasthttp.Response,
+	timeout time.Duration,
+) error {
+	span := c.startSpan(ctx, req)
+	err := c.client.DoTimeout(req, resp, timeout)
+	span.Finish(err, resp)
+
+	return err
+}
+
+type spanFinisher interface {
+	Finish(err error, resp *fasthttp.Response)
+}
+
+type dummyFinisher struct{}
+
+func (s dummyFinisher) Finish(error, *fasthttp.Response) {}
+
+type spanFinisherImpl struct {
+	span trace.Span
+}
+
+func (s spanFinisherImpl) Finish(err error, resp *fasthttp.Response) {
+	defer s.span.End()
+
+	if err != nil {
+		s.span.RecordError(err)
+		s.span.SetStatus(codes.Error, err.Error())
+
+		return
+	}
+
+	statusCode := resp.StatusCode()
+
+	s.span.SetAttributes(semconv.HTTPAttributesFromHTTPStatusCode(statusCode)...)
+	s.span.SetStatus(semconv.SpanStatusFromHTTPStatusCode(statusCode))
+}
+
+func (c *WrappedClient) startSpan(ctx context.Context, req *fasthttp.Request) spanFinisher {
+	var tracer trace.Tracer
+
+	if span := trace.SpanFromContext(ctx); span.SpanContext().IsValid() {
+		tracer = newTracer(span.TracerProvider())
+	} else {
+		tracer = newTracer(otel.GetTracerProvider())
+	}
+
+	httpReq, err := toHTTPRequest(req)
+	if err != nil {
+		return dummyFinisher{}
+	}
+
+	operationName := fmt.Sprintf("%s %s", string(req.Host()), string(req.URI().Path()))
+	ctx, span := tracer.Start(ctx, operationName,
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(semconv.HTTPClientAttributesFromHTTPRequest(httpReq)...))
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(httpReq.Header))
+
+	return spanFinisherImpl{
+		span: span,
+	}
+}
+
+func toHTTPRequest(req *fasthttp.Request) (*http.Request, error) {
+	rURL, err := url.ParseRequestURI(string(req.RequestURI()))
+	if err != nil {
+		return nil, err
+	}
+
+	body := req.Body()
+	r := &http.Request{}
+
+	r.Method = string(req.Header.Method())
+	r.Proto = "HTTP/1.1"
+	r.ProtoMajor = 1
+	r.ProtoMinor = 1
+	r.ContentLength = int64(len(body))
+	r.Host = string(req.URI().Host())
+	r.Body = io.NopCloser(bytes.NewReader(body))
+	r.URL = rURL
+
+	if r.Header == nil {
+		r.Header = make(http.Header)
+	} else if len(r.Header) > 0 {
+		for k := range r.Header {
+			delete(r.Header, k)
+		}
+	}
+
+	req.Header.VisitAll(func(k, v []byte) {
+		sk := string(k)
+		sv := string(v)
+
+		switch sk {
+		case "Transfer-Encoding":
+			r.TransferEncoding = append(r.TransferEncoding, sv)
+		default:
+			r.Header.Set(sk, sv)
+		}
+	})
+
+	return r, nil
+}
---
+++ internal/fasthttp/opentelemetry/client_test.go
@@ -0,0 +1,202 @@
+package opentelemetry
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/valyala/fasthttp"
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"go.opentelemetry.io/otel/trace"
+
+	otelmock "github.com/dadrus/heimdall/internal/x/opentelemetry/mock"
+)
+
+func TestWrappedClientDoTimeout(t *testing.T) {
+	t.Parallel()
+
+	mtracer := otelmock.NewMockTracer()
+	parentSpanContext := trace.NewSpanContext(trace.SpanContextConfig{
+		TraceID:    trace.TraceID{1},
+		SpanID:     trace.SpanID{2},
+		TraceFlags: trace.FlagsSampled,
+	})
+	ctxWithSpan, span := mtracer.Start(
+		trace.ContextWithSpanContext(context.Background(), parentSpanContext), "test")
+
+	defer span.End()
+
+	var responseCode int
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/foobar", r.URL.Path)
+		w.WriteHeader(responseCode)
+	}))
+	defer srv.Close()
+
+	srvURL, err := url.Parse(srv.URL)
+	require.NoError(t, err)
+
+	for _, tc := range []struct {
+		uc             string
+		tracer         *otelmock.MockTracer
+		timeout        time.Duration
+		ctx            context.Context // nolint: containedctx
+		serverResponse int
+		assert         func(t *testing.T, err error, req *fasthttp.Request, resp *fasthttp.Response,
+			spans []*otelmock.MockSpan)
+	}{
+		{
+			uc:             "request without parent span, resulting in http 200",
+			serverResponse: http.StatusOK,
+			ctx:            context.Background(),
+			timeout:        10 * time.Second,
+			assert: func(t *testing.T, err error, req *fasthttp.Request, resp *fasthttp.Response,
+				spans []*otelmock.MockSpan,
+			) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, http.StatusOK, resp.StatusCode())
+
+				assert.Empty(t, spans)
+			},
+		},
+		{
+			uc:             "request with parent span, resulting in http 200",
+			serverResponse: http.StatusOK,
+			ctx:            ctxWithSpan,
+			timeout:        10 * time.Second,
+			assert: func(t *testing.T, err error, req *fasthttp.Request, resp *fasthttp.Response,
+				spans []*otelmock.MockSpan,
+			) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, http.StatusOK, resp.StatusCode())
+
+				require.Len(t, spans, 1)
+				assert.NotEqual(t, parentSpanContext.SpanID(), spans[0].SpanContext().SpanID())
+				assert.Equal(t, parentSpanContext.TraceID(), spans[0].SpanContext().TraceID())
+
+				assert.Equal(t, fmt.Sprintf("%s /foobar", srvURL.Host), spans[0].Name)
+				assert.Equal(t, trace.SpanKindClient, spans[0].SpanKind)
+
+				attributes := spans[0].Attributes
+				assert.Len(t, attributes, 9)
+				assert.Contains(t, attributes, semconv.HTTPURLKey.String("/foobar"))
+				assert.Contains(t, attributes, semconv.HTTPUserAgentKey.String("test-client"))
+				assert.Contains(t, attributes, semconv.HTTPSchemeHTTP)
+				assert.Contains(t, attributes, semconv.HTTPHostKey.String(srvURL.Host))
+				assert.Contains(t, attributes, semconv.HTTPFlavorHTTP11)
+				assert.Contains(t, attributes, semconv.HTTPMethodKey.String("GET"))
+				assert.Contains(t, attributes, semconv.HTTPStatusCodeKey.Int64(200))
+				assert.Contains(t, attributes, attribute.Int64("status.code", 0))
+				assert.Contains(t, attributes, attribute.String("status.message", ""))
+			},
+		},
+		{
+			uc:             "request with parent span and error while communicating with the server",
+			serverResponse: http.StatusOK,
+			ctx:            ctxWithSpan,
+			timeout:        1 * time.Nanosecond,
+			assert: func(t *testing.T, err error, req *fasthttp.Request, resp *fasthttp.Response,
+				spans []*otelmock.MockSpan,
+			) {
+				t.Helper()
+
+				require.Error(t, err)
+
+				require.Len(t, spans, 1)
+				assert.NotEqual(t, parentSpanContext.SpanID(), spans[0].SpanContext().SpanID())
+				assert.Equal(t, parentSpanContext.TraceID(), spans[0].SpanContext().TraceID())
+
+				assert.Equal(t, fmt.Sprintf("%s /foobar", srvURL.Host), spans[0].Name)
+				assert.Equal(t, trace.SpanKindClient, spans[0].SpanKind)
+
+				attributes := spans[0].Attributes
+				assert.Len(t, attributes, 8)
+				assert.Contains(t, attributes, semconv.HTTPURLKey.String("/foobar"))
+				assert.Contains(t, attributes, semconv.HTTPUserAgentKey.String("test-client"))
+				assert.Contains(t, attributes, semconv.HTTPSchemeHTTP)
+				assert.Contains(t, attributes, semconv.HTTPHostKey.String(srvURL.Host))
+				assert.Contains(t, attributes, semconv.HTTPFlavorHTTP11)
+				assert.Contains(t, attributes, semconv.HTTPMethodKey.String("GET"))
+				assert.Contains(t, attributes, attribute.Int64("status.code", 1))
+				assert.Contains(t, attributes, attribute.String("status.message", "timeout"))
+			},
+		},
+		{
+			uc:             "request with parent span and error server response",
+			serverResponse: http.StatusInternalServerError,
+			ctx:            ctxWithSpan,
+			timeout:        10 * time.Second,
+			assert: func(t *testing.T, err error, req *fasthttp.Request, resp *fasthttp.Response,
+				spans []*otelmock.MockSpan,
+			) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, http.StatusInternalServerError, resp.StatusCode())
+
+				require.Len(t, spans, 1)
+				assert.NotEqual(t, parentSpanContext.SpanID(), spans[0].SpanContext().SpanID())
+				assert.Equal(t, parentSpanContext.TraceID(), spans[0].SpanContext().TraceID())
+
+				assert.Equal(t, fmt.Sprintf("%s /foobar", srvURL.Host), spans[0].Name)
+				assert.Equal(t, trace.SpanKindClient, spans[0].SpanKind)
+
+				attributes := spans[0].Attributes
+				assert.Len(t, attributes, 9)
+				assert.Contains(t, attributes, semconv.HTTPURLKey.String("/foobar"))
+				assert.Contains(t, attributes, semconv.HTTPUserAgentKey.String("test-client"))
+				assert.Contains(t, attributes, semconv.HTTPSchemeHTTP)
+				assert.Contains(t, attributes, semconv.HTTPHostKey.String(srvURL.Host))
+				assert.Contains(t, attributes, semconv.HTTPFlavorHTTP11)
+				assert.Contains(t, attributes, semconv.HTTPMethodKey.String("GET"))
+				assert.Contains(t, attributes, semconv.HTTPStatusCodeKey.Int64(500))
+				assert.Contains(t, attributes, attribute.Int64("status.code", 1))
+				assert.Contains(t, attributes, attribute.String("status.message", ""))
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// GIVEN
+			mtracer.Reset()
+
+			responseCode = tc.serverResponse
+
+			req := fasthttp.AcquireRequest()
+			defer fasthttp.ReleaseRequest(req)
+
+			uri := fasthttp.AcquireURI()
+			defer fasthttp.ReleaseURI(uri)
+			uri.SetHost(srvURL.Host)
+			uri.SetScheme(srvURL.Scheme)
+			uri.SetPath("/foobar")
+
+			req.Header.SetMethod("GET")
+			req.Header.SetHost(srvURL.Host)
+			req.Header.SetUserAgent("test-client")
+			req.SetURI(uri)
+
+			resp := fasthttp.AcquireResponse()
+			defer fasthttp.ReleaseResponse(resp)
+
+			client := NewClient(&fasthttp.Client{})
+
+			// WHEN
+			err = client.DoTimeout(tc.ctx, req, resp, tc.timeout)
+
+			// THEN
+			tc.assert(t, err, req, resp, mtracer.FinishedSpans)
+		})
+	}
+}
