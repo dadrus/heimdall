e9eb97cd2fd6195f3e7765cd1fbfc69092bdfa7e
Dimitrij Drus <dadrus@gmx.de>
2022-10-06 07:20:15 +0200

    code simplifications

--- internal/pipeline/errorhandlers/matcher/header_matcher.go
+++ internal/pipeline/errorhandlers/matcher/header_matcher.go
@@ -1,26 +1,15 @@
 package matcher
 
-import "strings"
+import (
+	"golang.org/x/exp/slices"
+)
 
 type HeaderMatcher map[string][]string
 
 func (hm HeaderMatcher) Match(headers map[string]string) bool {
 	for name, valueList := range hm {
 		headerVal, found := headers[name]
-		if !found {
-			return false
-		}
-
-		var ok bool
-
-		for _, val := range valueList {
-			ok = strings.Contains(headerVal, val)
-			if ok {
-				break
-			}
-		}
-
-		if !ok {
+		if !found || !slices.Contains(valueList, headerVal) {
 			return false
 		}
 	}
