f5fda0f7e6a22ec5deb3eb6608cb5023ca29d702
Dimitrij Drus <dadrus@gmx.de>
2022-10-11 13:09:29 +0200

    further http endpoint provider implementation

---
+++ internal/rules/provider/httpendpoint/config_decoder.go
@@ -0,0 +1,23 @@
+package httpendpoint
+
+import (
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/dadrus/heimdall/internal/endpoint"
+)
+
+func decodeConfig(input any, output any) error {
+	dec, err := mapstructure.NewDecoder(
+		&mapstructure.DecoderConfig{
+			DecodeHook: mapstructure.ComposeDecodeHookFunc(
+				endpoint.DecodeAuthenticationStrategyHookFunc(),
+			),
+			Result:      output,
+			ErrorUnused: true,
+		})
+	if err != nil {
+		return err
+	}
+
+	return dec.Decode(input)
+}
--- internal/rules/provider/httpendpoint/http_endpoint_provider.go
+++ internal/rules/provider/httpendpoint/http_endpoint_provider.go
@@ -1,14 +1,23 @@
 package httpendpoint
 
 import (
+	"context"
+
 	"github.com/rs/zerolog"
 	"go.uber.org/fx"
 
 	"github.com/dadrus/heimdall/internal/config"
+	"github.com/dadrus/heimdall/internal/endpoint"
+	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/rules/event"
+	"github.com/dadrus/heimdall/internal/x/errorchain"
 )
 
 type httpEndpointProvider struct {
+	endpoint     endpoint.Endpoint
+	watchChanges bool
+	queue        event.RuleSetChangedEventQueue
+	logger       zerolog.Logger
 }
 
 type registrationArguments struct {
@@ -24,7 +33,65 @@ func registerProvider(args registrationArguments, logger zerolog.Logger) error {
 		return nil
 	}
 
+	type Config struct {
+		Endpoint endpoint.Endpoint `mapstructure:"endpoint"`
+		Watch    bool              `mapstructure:"watch"`
+	}
+
+	var conf Config
+
+	if err := decodeConfig(args.Config.Rules.Providers.HTTPEndpoint, &conf); err != nil {
+		return errorchain.
+			NewWithMessage(heimdall.ErrConfiguration, "failed to decode http_endpoint rule provider config").
+			CausedBy(err)
+	}
+
+	provider, err := newProvider(conf.Endpoint, conf.Watch, args.Queue, logger)
+	if err != nil {
+		return err
+	}
+
+	args.Lifecycle.Append(
+		fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				return provider.Start()
+			},
+			OnStop: func(ctx context.Context) error {
+				return provider.Stop()
+			},
+		},
+	)
+
 	logger.Info().Str("_rule_provider_type", "http_endpoint").Msg("Rule provider configured.")
 
+	return nil
+}
+
+func newProvider(
+	endpoint endpoint.Endpoint,
+	watchChanges bool,
+	queue event.RuleSetChangedEventQueue,
+	logger zerolog.Logger,
+) (*httpEndpointProvider, error) {
+	if err := endpoint.Validate(); err != nil {
+		return nil, errorchain.
+			NewWithMessage(heimdall.ErrConfiguration,
+				"failed to validate http_endpoint rule provider endpoint configuration").
+			CausedBy(err)
+	}
+
+	return &httpEndpointProvider{
+		endpoint:     endpoint,
+		watchChanges: watchChanges,
+		queue:        queue,
+		logger:       logger,
+	}, nil
+}
+
+func (p *httpEndpointProvider) Start() error {
+	return nil
+}
+
+func (p *httpEndpointProvider) Stop() error {
 	return nil
 }
