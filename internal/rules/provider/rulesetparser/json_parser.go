1736638e2c4479ef46df722712e5154d23c0e646
Dimitrij Drus <dadrus@gmx.de>
2022-10-20 17:33:46 +0200

    file split into two one for yaml parser and one for json parser

---
+++ internal/rules/provider/rulesetparser/json_parser.go
@@ -0,0 +1,27 @@
+package rulesetparser
+
+import (
+	"errors"
+	"io"
+
+	"github.com/goccy/go-json"
+
+	"github.com/dadrus/heimdall/internal/config"
+)
+
+func ParseJSON(reader io.Reader) ([]config.RuleConfig, error) {
+	var rcs []config.RuleConfig
+
+	dec := json.NewDecoder(reader)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(&rcs); err != nil {
+		if errors.Is(err, io.EOF) {
+			return rcs, nil
+		}
+
+		return nil, err
+	}
+
+	return rcs, nil
+}
--- internal/rules/provider/rulesetparser/yaml_parser.go
+++ internal/rules/provider/rulesetparser/yaml_parser.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"io"
 
-	"github.com/goccy/go-json"
 	"gopkg.in/yaml.v3"
 
 	"github.com/dadrus/heimdall/internal/config"
@@ -15,23 +14,6 @@ func ParseYAML(reader io.Reader) ([]config.RuleConfig, error) {
 
 	dec := yaml.NewDecoder(reader)
 	dec.KnownFields(true)
-
-	if err := dec.Decode(&rcs); err != nil {
-		if errors.Is(err, io.EOF) {
-			return rcs, nil
-		}
-
-		return nil, err
-	}
-
-	return rcs, nil
-}
-
-func ParseJSON(reader io.Reader) ([]config.RuleConfig, error) {
-	var rcs []config.RuleConfig
-
-	dec := json.NewDecoder(reader)
-	dec.DisallowUnknownFields()
 
 	if err := dec.Decode(&rcs); err != nil {
 		if errors.Is(err, io.EOF) {
