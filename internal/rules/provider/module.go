9e8fd556f94776e76ed12d51fbb217a9a62b95b1
Dimitrij Drus <dadrus@gmx.de>
2022-10-22 12:33:03 +0200

    linter warnings resolved

--- internal/rules/provider/module.go
+++ internal/rules/provider/module.go
@@ -1,11 +1,11 @@
 package provider
 
 import (
-	"github.com/dadrus/heimdall/internal/rules/provider/cloudblob"
 	"github.com/rs/zerolog"
 	"go.uber.org/fx"
 
 	"github.com/dadrus/heimdall/internal/config"
+	"github.com/dadrus/heimdall/internal/rules/provider/cloudblob"
 	"github.com/dadrus/heimdall/internal/rules/provider/filesystem"
 	"github.com/dadrus/heimdall/internal/rules/provider/httpendpoint"
 )
