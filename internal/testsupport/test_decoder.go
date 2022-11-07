656820afe33477da3c710d7c29cc0a7b6ca769ba
Dimitrij Drus <dadrus@gmx.de>
2022-11-07 13:03:38 +0100

    test_decoder updated to convert map keys of type any to strings

--- internal/testsupport/test_decoder.go
+++ internal/testsupport/test_decoder.go
@@ -1,12 +1,15 @@
 package testsupport
 
 import (
+	"github.com/knadh/koanf/maps"
 	"gopkg.in/yaml.v3"
 )
 
 func DecodeTestConfig(data []byte) (map[string]any, error) {
 	var out map[string]any
 	err := yaml.Unmarshal(data, &out)
+
+	maps.IntfaceKeysToStrings(out)
 
 	return out, err
 }
