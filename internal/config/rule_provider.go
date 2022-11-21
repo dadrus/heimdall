b1a9eb054d20933accdbc1d003be476e1e45a4b0
Dimitrij Drus <dadrus@gmx.de>
2022-11-21 12:49:32 +0100

    config for kubernetes provider (operator to load CRDs)

--- internal/config/rule_provider.go
+++ internal/config/rule_provider.go
@@ -4,6 +4,7 @@ type RuleProviders struct {
 	FileSystem   *FileBasedRuleProviderConfig `koanf:"file_system,omitempty"`
 	HTTPEndpoint map[string]any               `koanf:"http_endpoint,omitempty"`
 	CloudBlob    map[string]any               `koanf:"cloud_blob,omitempty"`
+	Kubernetes   map[string]any               `koanf:"kubernetes,omitempty"`
 }
 
 type FileBasedRuleProviderConfig struct {
