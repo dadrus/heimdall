7086929dc162513682828db8175d43a0989e09b0
Dimitrij Drus <dadrus@gmx.de>
2022-11-29 20:18:43 +0100

    provider tests

--- internal/rules/provider/kubernetes/provider_test.go
+++ internal/rules/provider/kubernetes/provider_test.go
@@ -7,12 +7,78 @@ import (
 	"time"
 
 	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 
+	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/rules/event"
+	"github.com/dadrus/heimdall/internal/testsupport"
 )
+
+func TestNewProvider(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		uc     string
+		conf   []byte
+		assert func(t *testing.T, err error, prov *provider)
+	}{
+		{
+			uc:   "with unknown field",
+			conf: []byte(`foo: bar`),
+			assert: func(t *testing.T, err error, prov *provider) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.ErrorIs(t, err, heimdall.ErrConfiguration)
+				assert.Contains(t, err.Error(), "failed to decode")
+			},
+		},
+		{
+			uc:   "with empty configuration",
+			conf: []byte(`{}`),
+			assert: func(t *testing.T, err error, prov *provider) {
+				t.Helper()
+
+				require.NoError(t, err)
+				require.NotNil(t, prov)
+				assert.Equal(t, DefaultClass, prov.ac)
+				assert.Nil(t, prov.cancel)
+				assert.NotNil(t, prov.cl)
+			},
+		},
+		{
+			uc:   "with auth_class configured",
+			conf: []byte(`auth_class: foo`),
+			assert: func(t *testing.T, err error, prov *provider) {
+				t.Helper()
+
+				require.NoError(t, err)
+				require.NotNil(t, prov)
+				assert.Equal(t, "foo", prov.ac)
+				assert.Nil(t, prov.cancel)
+				assert.NotNil(t, prov.cl)
+			},
+		},
+	} {
+		t.Run("case="+tc.uc, func(t *testing.T) {
+			// GIVEN
+			providerConf, err := testsupport.DecodeTestConfig(tc.conf)
+			require.NoError(t, err)
+
+			queue := make(event.RuleSetChangedEventQueue, 10)
+
+			// WHEN
+			prov, err := newProvider(providerConf, &rest.Config{Host: "http://localhost:80001"}, queue, log.Logger)
+
+			// THEN
+			tc.assert(t, err, prov)
+		})
+	}
+}
 
 func TestFoo(t *testing.T) {
 	t.SkipNow()
@@ -29,7 +95,7 @@ func TestFoo(t *testing.T) {
 	err = prov.Start(context.Background())
 	require.NoError(t, err)
 
-	time.Sleep(15 * time.Second)
+	time.Sleep(150 * time.Second)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
