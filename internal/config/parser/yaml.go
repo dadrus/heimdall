3bb13af8283627fbff63445bbc4659c74f1eb9d2
Dimitrij Drus <dadrus@gmx.de>
2022-12-15 23:03:15 +0100

    env substitution support

--- internal/config/parser/yaml.go
+++ internal/config/parser/yaml.go
@@ -1,9 +1,12 @@
 package parser
 
 import (
+	"os"
+
+	"github.com/drone/envsubst/v2"
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
-	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/rawbytes"
 
 	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/x/errorchain"
@@ -12,7 +15,19 @@ import (
 func koanfFromYaml(configFile string) (*koanf.Koanf, error) {
 	parser := koanf.New(".")
 
-	if err := parser.Load(file.Provider(configFile), yaml.Parser()); err != nil {
+	raw, err := os.ReadFile(configFile)
+	if err != nil {
+		return nil, errorchain.NewWithMessagef(heimdall.ErrConfiguration,
+			"failed to read yaml config from %s", configFile).CausedBy(err)
+	}
+
+	content, err := envsubst.EvalEnv(string(raw))
+	if err != nil {
+		return nil, errorchain.NewWithMessagef(heimdall.ErrConfiguration,
+			"failed to parse yaml config from %s", configFile).CausedBy(err)
+	}
+
+	if err = parser.Load(rawbytes.Provider([]byte(content)), yaml.Parser()); err != nil {
 		return nil, errorchain.NewWithMessagef(heimdall.ErrConfiguration,
 			"failed to load yaml config from %s", configFile).CausedBy(err)
 	}
--- internal/config/parser/yaml_test.go
+++ internal/config/parser/yaml_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/knadh/koanf"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/x"
 )
 
 func TestKoanfFromYaml(t *testing.T) {
@@ -15,10 +17,11 @@ func TestKoanfFromYaml(t *testing.T) {
 	for _, tc := range []struct {
 		uc     string
 		config []byte
+		chmod  func(t *testing.T, file *os.File)
 		assert func(t *testing.T, err error, kanf *koanf.Koanf)
 	}{
 		{
-			uc: "valid content",
+			uc: "valid content without env substitution",
 			config: []byte(`
 some_string: foo
 someint: 3
@@ -42,7 +45,35 @@ nested_2:
 			},
 		},
 		{
-			uc:     "invalid content",
+			uc: "valid content with env substitution and templates",
+			config: []byte(`
+some_string: ${FOO}
+someint: 3
+nested1:
+  somebool: true
+  some_string: bar
+nested_2:
+  - somebool: false
+    some_string: '{ "name": {{ if $user_name }}{{ quote $user_name }}{{ else }}{{ quote $email }}{{ end }} }'
+`),
+			assert: func(t *testing.T, err error, konf *koanf.Koanf) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, "BAR", konf.Get("some_string"))
+				assert.Equal(t, 3, konf.Get("someint"))
+				assert.Equal(t, "bar", konf.Get("nested1.some_string"))
+				assert.Equal(t, true, konf.Get("nested1.somebool"))
+				assert.Len(t, konf.Get("nested_2"), 1)
+				assert.Contains(t, konf.Get("nested_2"),
+					map[string]any{
+						"some_string": `{ "name": {{ if $user_name }}{{ quote $user_name }}{{ else }}{{ quote $email }}{{ end }} }`,
+						"somebool":    false,
+					})
+			},
+		},
+		{
+			uc:     "invalid yaml content",
 			config: []byte("foobar"),
 			assert: func(t *testing.T, err error, kanf *koanf.Koanf) {
 				t.Helper()
@@ -51,9 +82,37 @@ nested_2:
 				assert.Contains(t, err.Error(), "failed to load")
 			},
 		},
+		{
+			uc:     "invalid yaml env substitution",
+			config: []byte("${:}"),
+			assert: func(t *testing.T, err error, kanf *koanf.Koanf) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), "failed to parse")
+			},
+		},
+		{
+			uc:     "can't read file",
+			config: []byte(`foo: bar`),
+			chmod: func(t *testing.T, file *os.File) {
+				t.Helper()
+
+				err := file.Chmod(0o222)
+				require.NoError(t, err)
+			},
+			assert: func(t *testing.T, err error, kanf *koanf.Koanf) {
+				t.Helper()
+
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), "failed to read")
+			},
+		},
 	} {
 		t.Run(tc.uc, func(t *testing.T) {
 			// GIVEN
+			chmod := x.IfThenElse(tc.chmod != nil, tc.chmod, func(t *testing.T, file *os.File) { t.Helper() })
+
 			tempFile, err := os.CreateTemp("", "config-test-*")
 			require.NoError(t, err)
 
@@ -64,6 +123,8 @@ nested_2:
 
 			_, err = tempFile.Write(tc.config)
 			require.NoError(t, err)
+
+			chmod(t, tempFile)
 
 			// WHEN
 			konf, err := koanfFromYaml(fileName)
