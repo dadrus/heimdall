457c3ece02091e6feb61e2b6ab64275a900c81cb
Dimitrij Drus <dadrus@gmx.de>
2022-12-15 20:31:38 +0100

    PR #378 reverted as it has broken template usage

--- internal/config/parser/yaml.go
+++ internal/config/parser/yaml.go
@@ -1,10 +1,9 @@
 package parser
 
 import (
-	"github.com/a8m/envsubst"
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
-	"github.com/knadh/koanf/providers/rawbytes"
+	"github.com/knadh/koanf/providers/file"
 
 	"github.com/dadrus/heimdall/internal/heimdall"
 	"github.com/dadrus/heimdall/internal/x/errorchain"
@@ -13,13 +12,7 @@ import (
 func koanfFromYaml(configFile string) (*koanf.Koanf, error) {
 	parser := koanf.New(".")
 
-	buf, err := envsubst.ReadFile(configFile)
-	if err != nil {
-		return nil, errorchain.NewWithMessagef(heimdall.ErrConfiguration,
-			"failed to read yaml config from %s", configFile).CausedBy(err)
-	}
-
-	if err = parser.Load(rawbytes.Provider(buf), yaml.Parser()); err != nil {
+	if err := parser.Load(file.Provider(configFile), yaml.Parser()); err != nil {
 		return nil, errorchain.NewWithMessagef(heimdall.ErrConfiguration,
 			"failed to load yaml config from %s", configFile).CausedBy(err)
 	}
