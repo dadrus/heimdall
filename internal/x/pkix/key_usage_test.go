acaa66f85965352a21d8c03f5d2bfb76c3cfe897
Dimitrij Drus <dadrus@gmx.de>
2022-12-22 13:38:37 +0100

    renamings and more tests

---
+++ internal/x/pkix/key_usage_test.go
@@ -0,0 +1,29 @@
+package pkix
+
+import (
+	"crypto/x509"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeyUsageString(t *testing.T) {
+	for _, tc := range []struct {
+		usage x509.KeyUsage
+		name  string
+	}{
+		{usage: x509.KeyUsageDigitalSignature, name: "DigitalSignature"},
+		{usage: x509.KeyUsageContentCommitment, name: "ContentCommitment"},
+		{usage: x509.KeyUsageKeyEncipherment, name: "KeyEncipherment"},
+		{usage: x509.KeyUsageDataEncipherment, name: "DataEncipherment"},
+		{usage: x509.KeyUsageKeyAgreement, name: "KeyAgreement"},
+		{usage: x509.KeyUsageCertSign, name: "CertSign"},
+		{usage: x509.KeyUsageCRLSign, name: "CRLSign"},
+		{usage: x509.KeyUsageEncipherOnly, name: "EncipherOnly"},
+		{usage: x509.KeyUsageDecipherOnly, name: "DecipherOnly"},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.name, KeyUsage(tc.usage).String())
+		})
+	}
+}
