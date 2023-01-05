6cea6c0238c885e28c4c701431f013e3560e5eaa
Dimitrij Drus <dadrus@gmx.de>
2023-01-05 02:04:04 +0100

    certmon module registered for DI

--- internal/module.go
+++ internal/module.go
@@ -20,6 +20,7 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/dadrus/heimdall/internal/cache"
+	"github.com/dadrus/heimdall/internal/certmon"
 	"github.com/dadrus/heimdall/internal/config"
 	"github.com/dadrus/heimdall/internal/handler/management"
 	"github.com/dadrus/heimdall/internal/handler/metrics"
@@ -43,4 +44,5 @@ var Module = fx.Options(
 	rules.Module,
 	management.Module,
 	metrics.Module,
+	certmon.Module,
 )
