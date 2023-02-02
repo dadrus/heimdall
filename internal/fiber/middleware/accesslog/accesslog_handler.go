94bdc35eab96bef0fceeb8f38396f855979090b4
Dimitrij Drus <dadrus@gmx.de>
2023-02-02 14:26:38 +0100

    access logger implementation simplified

--- internal/fiber/middleware/accesslog/accesslog_handler.go
+++ internal/fiber/middleware/accesslog/accesslog_handler.go
@@ -17,6 +17,7 @@
 package accesslog
 
 import (
+	"context"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -29,38 +30,69 @@ import (
 func New(logger zerolog.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		start := time.Now()
-		traceCtx := tracecontext.Extract(c.UserContext())
+
 		c.SetUserContext(accesscontext.New(c.UserContext()))
 
-		accLog := createAccessLogger(c, logger, start, traceCtx)
+		logCtx := logger.Level(zerolog.InfoLevel).With().
+			Int64("_tx_start", start.Unix()).
+			Str("_client_ip", c.IP()).
+			Str("_http_method", c.Method()).
+			Str("_http_path", c.Path()).
+			Str("_http_user_agent", c.Get("User-Agent")).
+			Str("_http_host", string(c.Request().URI().Host())).
+			Str("_http_scheme", string(c.Request().URI().Scheme()))
+		logCtx = logTraceData(c.UserContext(), logCtx)
+
+		if c.IsProxyTrusted() {
+			logCtx = logHeader(c, logCtx, "X-Forwarded-Proto", "_http_x_forwarded_proto")
+			logCtx = logHeader(c, logCtx, "X-Forwarded-Host", "_http_x_forwarded_host")
+			logCtx = logHeader(c, logCtx, "X-Forwarded-Path", "_http_x_forwarded_path")
+			logCtx = logHeader(c, logCtx, "X-Forwarded-Uri", "_http_x_forwarded_uri")
+			logCtx = logHeader(c, logCtx, "X-Forwarded-For", "_http_x_forwarded_for")
+			logCtx = logHeader(c, logCtx, "Forwarded", "_http_forwarded")
+		}
+
+		accLog := logCtx.Logger()
 		accLog.Info().Msg("TX started")
 
 		err := c.Next()
 
-		createAccessLogFinalizationEvent(c, accLog, err, start, traceCtx).Msg("TX finished")
+		logAccessStatus(c.UserContext(), accLog.Info(), err).
+			Int("_body_bytes_sent", len(c.Response().Body())).
+			Int("_http_status_code", c.Response().StatusCode()).
+			Int64("_tx_duration_ms", time.Until(start).Milliseconds()).
+			Msg("TX finished")
 
 		return err
 	}
 }
 
-func createAccessLogger(
-	c *fiber.Ctx,
-	logger zerolog.Logger,
-	start time.Time,
-	traceCtx *tracecontext.TraceContext,
-) zerolog.Logger {
-	startTime := start.Unix()
+func logAccessStatus(ctx context.Context, event *zerolog.Event, err error) *zerolog.Event {
+	subject := accesscontext.Subject(ctx)
+	accessErr := accesscontext.Error(ctx)
 
-	logCtx := logger.Level(zerolog.InfoLevel).With().
-		Int64("_tx_start", startTime).
-		Str("_client_ip", c.IP()).
-		Str("_http_method", c.Method()).
-		Str("_http_path", c.Path()).
-		Str("_http_user_agent", c.Get("User-Agent")).
-		Str("_http_host", string(c.Request().URI().Host())).
-		Str("_http_scheme", string(c.Request().URI().Scheme()))
+	switch {
+	case err != nil:
+		if len(subject) != 0 {
+			event.Str("_subject", subject)
+		}
 
-	if traceCtx != nil {
+		event.Err(err).Bool("_access_granted", false)
+	case accessErr != nil:
+		if len(subject) != 0 {
+			event.Str("_subject", subject)
+		}
+
+		event.Err(accessErr).Bool("_access_granted", false)
+	default:
+		event.Str("_subject", subject).Bool("_access_granted", true)
+	}
+
+	return event
+}
+
+func logTraceData(ctx context.Context, logCtx zerolog.Context) zerolog.Context {
+	if traceCtx := tracecontext.Extract(ctx); traceCtx != nil {
 		logCtx = logCtx.
 			Str("_trace_id", traceCtx.TraceID).
 			Str("_span_id", traceCtx.SpanID)
@@ -70,78 +102,13 @@ func createAccessLogger(
 		}
 	}
 
-	if c.IsProxyTrusted() { // nolint: nestif
-		if headerValue := c.Get("X-Forwarded-Proto"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_x_forwarded_proto", headerValue)
-		}
-
-		if headerValue := c.Get("X-Forwarded-Host"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_x_forwarded_host", headerValue)
-		}
-
-		if headerValue := c.Get("X-Forwarded-Path"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_x_forwarded_path", headerValue)
-		}
-
-		if headerValue := c.Get("X-Forwarded-Uri"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_x_forwarded_uri", headerValue)
-		}
-
-		if headerValue := c.Get("X-Forwarded-For"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_x_forwarded_for", headerValue)
-		}
-
-		if headerValue := c.Get("Forwarded"); len(headerValue) != 0 {
-			logCtx = logCtx.Str("_http_forwarded", headerValue)
-		}
-	}
-
-	return logCtx.Logger()
+	return logCtx
 }
 
-func createAccessLogFinalizationEvent(
-	c *fiber.Ctx,
-	accessLogger zerolog.Logger,
-	err error,
-	start time.Time,
-	traceCtx *tracecontext.TraceContext,
-) *zerolog.Event {
-	end := time.Now()
-	duration := end.Sub(start)
-	subject := accesscontext.Subject(c.UserContext())
-	accessErr := accesscontext.Error(c.UserContext())
-
-	event := accessLogger.Info().
-		Int("_body_bytes_sent", len(c.Response().Body())).
-		Int("_http_status_code", c.Response().StatusCode()).
-		Int64("_tx_duration_ms", duration.Milliseconds())
-
-	if traceCtx != nil {
-		event = event.
-			Str("_trace_id", traceCtx.TraceID).
-			Str("_span_id", traceCtx.SpanID)
-
-		if len(traceCtx.ParentID) != 0 {
-			event = event.Str("_parent_id", traceCtx.ParentID)
-		}
+func logHeader(c *fiber.Ctx, logCtx zerolog.Context, headerName, logKey string) zerolog.Context {
+	if headerValue := c.Get(headerName); len(headerValue) != 0 {
+		logCtx = logCtx.Str(logKey, headerValue)
 	}
 
-	switch {
-	case err != nil:
-		if len(subject) != 0 {
-			event = event.Str("_subject", subject)
-		}
-
-		event = event.Err(err).Bool("_access_granted", false)
-	case accessErr != nil:
-		if len(subject) != 0 {
-			event = event.Str("_subject", subject)
-		}
-
-		event = event.Err(accessErr).Bool("_access_granted", false)
-	default:
-		event = event.Str("_subject", subject).Bool("_access_granted", true)
-	}
-
-	return event
+	return logCtx
 }
--- internal/handler/envoyextauth/grpcv3/middleware/accesslog/accesslog_handler.go
+++ internal/handler/envoyextauth/grpcv3/middleware/accesslog/accesslog_handler.go
@@ -18,10 +18,13 @@ package accesslog
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 
 	"github.com/dadrus/heimdall/internal/accesscontext"
 	"github.com/dadrus/heimdall/internal/x/opentelemetry/tracecontext"
@@ -30,33 +33,57 @@ import (
 func New(logger zerolog.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		start := time.Now()
-		traceCtx := tracecontext.Extract(ctx)
-		ctx = accesscontext.New(ctx)
+		requestMetadata, _ := metadata.FromIncomingContext(ctx)
 
-		accLog := createAccessLogger(logger, start, traceCtx)
+		logCtx := logger.Level(zerolog.InfoLevel).With().
+			Int64("_tx_start", start.Unix()).
+			Str("_peer", peerFromCtx(ctx)).
+			Str("_request", info.FullMethod)
+
+		logCtx = logTraceData(ctx, logCtx)
+		logCtx = md(logCtx, requestMetadata, "x-forwarded-for", "_x_forwarded_for")
+		logCtx = md(logCtx, requestMetadata, "forwarded", "_forwarded")
+
+		accLog := logCtx.Logger()
 		accLog.Info().Msg("TX started")
 
+		ctx = accesscontext.New(ctx)
 		res, err := handler(ctx, req)
 
-		createAccessLogFinalizationEvent(ctx, accLog, err, start, traceCtx).Msg("TX finished")
+		logAccessStatus(ctx, accLog.Info(), err).
+			Int64("_tx_duration_ms", time.Until(start).Milliseconds()).
+			Msg("TX finished")
 
 		return res, err
 	}
 }
 
-func createAccessLogger(logger zerolog.Logger, start time.Time, traceCtx *tracecontext.TraceContext) zerolog.Logger {
-	startTime := start.Unix()
+func logAccessStatus(ctx context.Context, event *zerolog.Event, err error) *zerolog.Event {
+	subject := accesscontext.Subject(ctx)
+	accessErr := accesscontext.Error(ctx)
 
-	logCtx := logger.Level(zerolog.InfoLevel).With().
-		Int64("_tx_start", startTime)
-	// Str("_client_ip", c.IP()).
-	// Str("_http_method", c.Method()).
-	// Str("_http_path", c.Path()).
-	// Str("_http_user_agent", c.Get("User-Agent")).
-	// Str("_http_host", string(c.Request().URI().Host())).
-	// Str("_http_scheme", string(c.Request().URI().Scheme()))
+	switch {
+	case err != nil:
+		if len(subject) != 0 {
+			event.Str("_subject", subject)
+		}
 
-	if traceCtx != nil {
+		event.Err(err).Bool("_access_granted", false)
+	case accessErr != nil:
+		if len(subject) != 0 {
+			event.Str("_subject", subject)
+		}
+
+		event.Err(accessErr).Bool("_access_granted", false)
+	default:
+		event.Str("_subject", subject).Bool("_access_granted", true)
+	}
+
+	return event
+}
+
+func logTraceData(ctx context.Context, logCtx zerolog.Context) zerolog.Context {
+	if traceCtx := tracecontext.Extract(ctx); traceCtx != nil {
 		logCtx = logCtx.
 			Str("_trace_id", traceCtx.TraceID).
 			Str("_span_id", traceCtx.SpanID)
@@ -66,78 +93,22 @@ func createAccessLogger(logger zerolog.Logger, start time.Time, traceCtx *tracec
 		}
 	}
 
-	// if c.IsProxyTrusted() { // nolint: nestif
-	//     if headerValue := c.Get("X-Forwarded-Proto"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_x_forwarded_proto", headerValue)
-	//     }
-	//
-	//     if headerValue := c.Get("X-Forwarded-Host"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_x_forwarded_host", headerValue)
-	//     }
-	//
-	//     if headerValue := c.Get("X-Forwarded-Path"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_x_forwarded_path", headerValue)
-	//     }
-	//
-	//     if headerValue := c.Get("X-Forwarded-Uri"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_x_forwarded_uri", headerValue)
-	//     }
-	//
-	//     if headerValue := c.Get("X-Forwarded-For"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_x_forwarded_for", headerValue)
-	//     }
-	//
-	//     if headerValue := c.Get("Forwarded"); len(headerValue) != 0 {
-	//         logCtx = logCtx.Str("_http_forwarded", headerValue)
-	//     }
-	// }
-
-	return logCtx.Logger()
+	return logCtx
 }
 
-func createAccessLogFinalizationEvent(
-	ctx context.Context,
-	accessLogger zerolog.Logger,
-	err error,
-	start time.Time,
-	traceCtx *tracecontext.TraceContext,
-) *zerolog.Event {
-	end := time.Now()
-	duration := end.Sub(start)
-	subject := accesscontext.Subject(ctx)
-	accessErr := accesscontext.Error(ctx)
-
-	event := accessLogger.Info().
-		// Int("_body_bytes_sent", len(c.Response().Body())).
-		// Int("_http_status_code", c.Response().StatusCode()).
-		Int64("_tx_duration_ms", duration.Milliseconds())
-
-	if traceCtx != nil {
-		event = event.
-			Str("_trace_id", traceCtx.TraceID).
-			Str("_span_id", traceCtx.SpanID)
-
-		if len(traceCtx.ParentID) != 0 {
-			event = event.Str("_parent_id", traceCtx.ParentID)
-		}
+func md(logCtx zerolog.Context, rmd metadata.MD, mdKey, logKey string) zerolog.Context {
+	if headerValue := rmd.Get(mdKey); len(headerValue) != 0 {
+		logCtx = logCtx.Str(logKey, strings.Join(headerValue, ","))
 	}
 
-	switch {
-	case err != nil:
-		if len(subject) != 0 {
-			event = event.Str("_subject", subject)
-		}
+	return logCtx
+}
 
-		event = event.Err(err).Bool("_access_granted", false)
-	case accessErr != nil:
-		if len(subject) != 0 {
-			event = event.Str("_subject", subject)
-		}
-
-		event = event.Err(accessErr).Bool("_access_granted", false)
-	default:
-		event = event.Str("_subject", subject).Bool("_access_granted", true)
+func peerFromCtx(ctx context.Context) string {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return ""
 	}
 
-	return event
+	return p.Addr.String()
 }
