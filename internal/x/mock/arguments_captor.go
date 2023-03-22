bbdd4fb1113357eddfad57a8813a6d3852d3830a
Dimitrij Drus <dadrus@gmx.de>
2023-03-22 11:10:43 +0100

    argument captors to capture arguments received by the mocked method calls

---
+++ internal/x/mock/arguments_captor.go
@@ -0,0 +1,31 @@
+package mock
+
+import "github.com/stretchr/testify/mock"
+
+type ArgumentsCaptor struct {
+	capturedArgs []mock.Arguments
+}
+
+func NewArgumentsCaptor(m *mock.Mock, name string) *ArgumentsCaptor {
+	captor := &ArgumentsCaptor{}
+
+	m.TestData().Set(name, captor)
+
+	return captor
+}
+
+func (c *ArgumentsCaptor) Capture(args mock.Arguments) {
+	c.capturedArgs = append(c.capturedArgs, args)
+}
+
+func (c *ArgumentsCaptor) Values(call int) mock.Arguments {
+	if len(c.capturedArgs)-1 >= call {
+		return c.capturedArgs[call]
+	}
+
+	return nil
+}
+
+func ArgumentsCaptorFrom(m *mock.Mock, name string) *ArgumentsCaptor {
+	return m.TestData().Get(name).Data().(*ArgumentsCaptor)
+}
