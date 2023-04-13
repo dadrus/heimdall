4912ef806b95a7227cdd6e9cf1cc3a80fbd150c9
Dimitrij Drus <dadrus@gmx.de>
2023-04-13 15:46:38 +0200

    test renamed

--- cmd/validate/config_test.go
+++ cmd/validate/config_test.go
@@ -47,7 +47,7 @@ func TestValidateConfig(t *testing.T) {
 	}
 }
 
-func TestRunValidateCommand(t *testing.T) {
+func TestRunValidateConfigCommand(t *testing.T) {
 	t.Parallel()
 
 	for _, tc := range []struct {
