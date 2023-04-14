ef9b88ff69adf4ebe656c36df371e78ca25f315c
Dimitrij Drus <dadrus@gmx.de>
2023-04-14 14:44:43 +0200

    more tests

--- internal/rules/cel_execution_condition_test.go
+++ internal/rules/cel_execution_condition_test.go
@@ -1,10 +1,15 @@
 package rules
 
 import (
+	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/heimdall/mocks"
+	"github.com/dadrus/heimdall/internal/rules/mechanisms/subject"
 )
 
 func TestNewCelExecutionCondition(t *testing.T) {
@@ -32,6 +37,67 @@ func TestNewCelExecutionCondition(t *testing.T) {
 				require.NotNil(t, condition)
 				require.NotNil(t, condition.p)
 			}
+		})
+	}
+}
+
+func TestCelExecutionConditionCanExecute(t *testing.T) {
+	t.Parallel()
+
+	sub := &subject.Subject{
+		ID: "foobar",
+		Attributes: map[string]any{
+			"group1": []string{"admin@acme.co", "analyst@acme.co"},
+			"labels": []string{"metadata", "prod", "pii"},
+			"groupN": []string{"forever@acme.co"},
+		},
+	}
+
+	for _, tc := range []struct {
+		uc         string
+		expression string
+		expected   bool
+	}{
+		{
+			uc: "complex expression evaluating to true",
+			expression: `Subject.Attributes.exists(c, c.startsWith('group'))
+							&& Subject.Attributes.filter(c, c.startsWith('group'))
+								.all(c, Subject.Attributes[c].all(g, g.endsWith('@acme.co')))`,
+			expected: true,
+		},
+		{
+			uc:         "simple expression evaluating to false",
+			expression: `Subject.ID == "anonymous" && Request.Method == "GET"`,
+			expected:   false,
+		},
+		{
+			uc:         "simple expression evaluating to true",
+			expression: `Subject.ID == "foobar" && Request.Method == "GET"`,
+			expected:   true,
+		},
+	} {
+		t.Run(tc.uc, func(t *testing.T) {
+			// GIVEN
+			ctx := mocks.NewContextMock(t)
+
+			ctx.EXPECT().RequestURL().Return(&url.URL{
+				Scheme:   "http",
+				Host:     "localhost",
+				Path:     "/test",
+				RawQuery: "foo=bar&baz=zab",
+			})
+			ctx.EXPECT().RequestMethod().Return(http.MethodGet)
+			ctx.EXPECT().RequestClientIPs().Return([]string{"127.0.0.1", "10.10.10.10"})
+
+			condition, err := newCelExecutionCondition(tc.expression)
+			require.NoError(t, err)
+
+			// WHEN
+			can, err := condition.CanExecute(ctx, sub)
+
+			// THEN
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, can)
 		})
 	}
 }
