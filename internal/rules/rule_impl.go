b5c56753416d906a0c647933e3ecf6ec6ec801b9
Dimitrij Drus <dadrus@gmx.de>
2023-06-24 20:02:08 +0200

    fix: Matcher expressions do not have to cope with url encoded path fragments any more if such are present

--- internal/rules/rule_impl.go
+++ internal/rules/rule_impl.go
@@ -17,6 +17,7 @@
 package rules
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/rs/zerolog"
@@ -75,8 +76,10 @@ func (r *ruleImpl) Execute(ctx heimdall.Context) (*url.URL, error) {
 }
 
 func (r *ruleImpl) MatchesURL(requestURL *url.URL) bool {
-	toBeMatched := *requestURL
-	toBeMatched.RawQuery = ""
+	toBeMatched := url.URL{
+		Scheme: requestURL.Scheme,
+		Opaque: fmt.Sprintf("//%s%s", requestURL.Host, requestURL.Path),
+	}
 
 	return r.urlMatcher.Match(toBeMatched.String())
 }
