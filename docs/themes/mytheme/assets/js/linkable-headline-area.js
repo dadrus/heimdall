179f39753c8cb24ac17579fc497f73886772a3ce
Dimitrij Drus <dadrus@gmx.de>
2023-08-09 12:58:37 +0200

    chore: Chapters within sections are now linkable (#850)

---
+++ docs/themes/mytheme/assets/js/linkable-headline-area.js
@@ -0,0 +1,24 @@
+class LinkableContentArea extends HTMLElement {
+    connectedCallback() {
+        const headlines = Array.from(this.querySelectorAll('h1,h2,h3,h4,h5,h6'))
+
+        for (const headline of headlines) {
+            if (headline.hasAttribute('id')) {
+                const l = link(`#${headline.getAttribute('id')}`)
+                headline.insertAdjacentElement('beforeend', l)
+            }
+        }
+    }
+}
+
+customElements.define('linkable-headline-area', LinkableContentArea)
+
+
+function link(href) {
+    const a = document.createElement('a')
+    a.setAttribute('href', href)
+    a.classList.add('reference-link')
+    a.textContent = '#'
+    console.log(a)
+    return a
+}
