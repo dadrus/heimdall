f3c6ff05b69bf1588426a7c923c3786f5a997655
Dimitrij Drus <dadrus@gmx.de>
2023-08-29 18:57:42 +0200

    added forgotten method and url extraction

--- internal/handler/proxy2/middlewares/trustedproxy/handler.go
+++ internal/handler/proxy2/middlewares/trustedproxy/handler.go
@@ -1,11 +1,15 @@
 package trustedproxy
 
 import (
+	"context"
 	"net"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/gofiber/fiber/v2/log"
+
+	"github.com/dadrus/heimdall/internal/x"
 )
 
 var untrustedHeader = []string{ //nolint:gochecknoglobals
@@ -17,6 +21,11 @@ var untrustedHeader = []string{ //nolint:gochecknoglobals
 	"X-Forwarded-Path",
 	"X-Forwarded-Method",
 }
+
+type (
+	methodKey     struct{}
+	requestURLKey struct{}
+)
 
 type ipHolder interface {
 	Contains(ip net.IP) bool
@@ -65,7 +74,13 @@ func New(proxies ...string) func(http.Handler) http.Handler {
 				dropUntrustedHeaders(req)
 			}
 
-			next.ServeHTTP(rw, req)
+			method := requestMethod(req)
+			reqURL := requestURL(req)
+
+			ctx := context.WithValue(req.Context(), methodKey{}, method)
+			ctx = context.WithValue(ctx, requestURLKey{}, reqURL)
+
+			next.ServeHTTP(rw, req.WithContext(ctx))
 		})
 	}
 }
@@ -74,4 +89,34 @@ func dropUntrustedHeaders(req *http.Request) {
 	for _, name := range untrustedHeader {
 		req.Header.Del(name)
 	}
+}
+
+// RequestMethod returns the HTTP method associated with the ctx. If no method is associated,
+// an empty string is returned.
+func RequestMethod(ctx context.Context) string {
+	var (
+		method string
+		ok     bool
+	)
+
+	if val := ctx.Value(methodKey{}); val != nil {
+		method, ok = val.(string)
+	}
+
+	return x.IfThenElse(ok, method, "")
+}
+
+// RequestURL returns the URL associated with the ctx. If no URL is associated,
+// nil is returned.
+func RequestURL(ctx context.Context) *url.URL {
+	var (
+		reqURL *url.URL
+		ok     bool
+	)
+
+	if val := ctx.Value(requestURLKey{}); val != nil {
+		reqURL, ok = val.(*url.URL)
+	}
+
+	return x.IfThenElse(ok, reqURL, nil)
 }
---
+++ internal/handler/proxy2/middlewares/trustedproxy/request_url.go
@@ -0,0 +1,70 @@
+// Copyright 2022 Dimitrij Drus <dadrus@gmx.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package trustedproxy
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func requestURL(req *http.Request) *url.URL {
+	var (
+		rawPath string
+		path    string
+		query   string
+	)
+
+	proto := req.Header.Get("X-Forwarded-Proto")
+	if len(proto) == 0 {
+		proto = req.Proto
+	}
+
+	host := req.Header.Get("X-Forwarded-Host")
+	if len(host) == 0 {
+		host = req.URL.Host
+	}
+
+	if val := req.Header.Get("X-Forwarded-Uri"); len(val) != 0 {
+		if forwardedURI, err := url.Parse(val); err != nil {
+			rawPath = forwardedURI.Path
+			query = forwardedURI.Query().Encode()
+		}
+	} else {
+		rawPath = req.Header.Get("X-Forwarded-Path")
+	}
+
+	if len(rawPath) == 0 {
+		rawPath = req.URL.RawPath
+		if len(rawPath) != 0 {
+			rawPath = fmt.Sprintf("/%s", rawPath)
+		}
+	}
+
+	if len(query) == 0 {
+		query = req.URL.RawQuery
+	}
+
+	path, _ = url.PathUnescape(rawPath)
+
+	return &url.URL{
+		Scheme:   proto,
+		Host:     host,
+		Path:     path,
+		RawQuery: query,
+	}
+}
