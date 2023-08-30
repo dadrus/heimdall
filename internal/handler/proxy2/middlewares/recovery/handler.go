1715c9974f011b3d7439a9474e6a30cee48e5243
Dimitrij Drus <dadrus@gmx.de>
2023-08-30 16:02:09 +0200

    new tests for recovery handler and fixes

--- internal/handler/proxy2/middlewares/recovery/handler.go
+++ internal/handler/proxy2/middlewares/recovery/handler.go
@@ -13,17 +13,23 @@ import (
 	"github.com/dadrus/heimdall/internal/x/stringx"
 )
 
-func New(eh *errorhandler.ErrorHandler) func(http.Handler) http.Handler {
+func New(eh errorhandler.ErrorHandler) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			defer func() {
 				if rec := recover(); rec != nil {
 					zerolog.Ctx(req.Context()).Error().Msg(fmt.Sprintf("%v\n%s", rec, stringx.ToString(debug.Stack())))
 
-					// rec is always of type error here
-					// nolint: forcetypeassert
-					err := errorchain.NewWithMessage(heimdall.ErrInternal, "runtime error occurred").
-						CausedBy(rec.(error))
+					err, ok := rec.(error)
+					if ok {
+						err = errorchain.NewWithMessage(heimdall.ErrInternal, "runtime error occurred").
+							CausedBy(err)
+					} else {
+						// nolint: goerr113
+						err = errorchain.NewWithMessage(heimdall.ErrInternal, "runtime error occurred").
+							CausedBy(fmt.Errorf("%v", rec))
+					}
+
 					eh.HandleError(rw, req, err)
 				}
 			}()
---
+++ internal/handler/proxy2/middlewares/recovery/handler_test.go
@@ -0,0 +1,74 @@
+package recovery
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/justinas/alice"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/handler/proxy2/middlewares/errorhandler/mocks"
+)
+
+func TestHeaderExecution(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		uc          string
+		shouldPanic bool
+		err         any
+	}{
+		{"panics with string as error", true, "string error"},
+		{"panics with real error type", true, errors.New("err error")},
+		{"does not panic", false, ""},
+	} {
+		t.Run(tc.uc, func(t *testing.T) {
+			// GIVEN
+			eh := mocks.NewErrorHandlerMock(t)
+			srv := httptest.NewServer(
+				alice.New(New(eh)).
+					ThenFunc(func(rw http.ResponseWriter, req *http.Request) {
+						if tc.shouldPanic {
+							eh.EXPECT().HandleError(mock.Anything, mock.Anything, mock.Anything).Run(
+								func(rw http.ResponseWriter, req *http.Request, err error) {
+									rw.WriteHeader(http.StatusInsufficientStorage)
+								})
+
+							panic(tc.err)
+						}
+
+						rw.WriteHeader(http.StatusOK)
+					}))
+
+			defer srv.Close()
+
+			req, err := http.NewRequestWithContext(
+				context.Background(), http.MethodGet, fmt.Sprintf("%s/test", srv.URL), nil)
+			require.NoError(t, err)
+
+			// WHEN
+			resp, err := srv.Client().Do(req)
+
+			// THEN
+			require.NoError(t, err)
+			defer resp.Body.Close()
+
+			res, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.Empty(t, res)
+
+			if tc.shouldPanic {
+				assert.Equal(t, http.StatusInsufficientStorage, resp.StatusCode)
+			} else {
+				assert.Equal(t, http.StatusOK, resp.StatusCode)
+			}
+		})
+	}
+}
