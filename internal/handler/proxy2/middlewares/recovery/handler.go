c3164885cf15ed1ba93c673d04cd453686efe526
Dimitrij Drus <dadrus@gmx.de>
2023-08-30 11:08:42 +0200

    recovery handler refactored to support better responses

--- internal/handler/proxy2/middlewares/recovery/handler.go
+++ internal/handler/proxy2/middlewares/recovery/handler.go
@@ -7,19 +7,21 @@ import (
 
 	"github.com/rs/zerolog"
 
+	"github.com/dadrus/heimdall/internal/handler/proxy2/middlewares/errorhandler"
+	"github.com/dadrus/heimdall/internal/heimdall"
+	"github.com/dadrus/heimdall/internal/x/errorchain"
 	"github.com/dadrus/heimdall/internal/x/stringx"
 )
 
-func New() func(http.Handler) http.Handler {
+func New(eh *errorhandler.ErrorHandler) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			logger := zerolog.Ctx(req.Context())
-
 			defer func() {
 				if rec := recover(); rec != nil {
-					logger.Error().Msg(fmt.Sprintf("%v\n%s", rec, stringx.ToString(debug.Stack())))
+					zerolog.Ctx(req.Context()).Error().Msg(fmt.Sprintf("%v\n%s", rec, stringx.ToString(debug.Stack())))
 
-					rw.WriteHeader(http.StatusInternalServerError)
+					// rec is always of type error here
+					eh.HandleError(rw, req, errorchain.New(heimdall.ErrInternal).CausedBy(rec.(error))) // nolint: forcetypeassert
 				}
 			}()
 
