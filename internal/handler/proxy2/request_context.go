549574afd878ef16c2e0368cbebac095a3e08c12
Dimitrij Drus <dadrus@gmx.de>
2023-09-22 10:14:52 +0200

    making use of the new connections limits config

--- internal/handler/proxy2/request_context.go
+++ internal/handler/proxy2/request_context.go
@@ -68,8 +68,8 @@ func newRequestContextFactory(signer heimdall.JWTSigner, cfg config.ServiceConfi
 			Timeout:   30 * time.Second, //nolint:gomnd
 			KeepAlive: 30 * time.Second, //nolint:gomnd
 		}).DialContext,
-		MaxIdleConns:          100, //nolint:gomnd
-		MaxIdleConnsPerHost:   50,  //nolint:gomnd
+		MaxIdleConns:          cfg.ConnectionsLimit.MaxIdle,
+		MaxIdleConnsPerHost:   cfg.ConnectionsLimit.MaxIdlePerHost,
 		IdleConnTimeout:       cfg.Timeout.Idle,
 		TLSHandshakeTimeout:   10 * time.Second, //nolint:gomnd
 		ExpectContinueTimeout: 1 * time.Second,
