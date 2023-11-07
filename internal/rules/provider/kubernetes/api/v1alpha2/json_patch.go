835570276bcd3b4f9e9bd2be37f6cddc54c2f542
Dimitrij Drus <dadrus@gmx.de>
2023-11-07 12:45:16 +0100

    jsonPatch constructor function changed

--- internal/rules/provider/kubernetes/api/v1alpha2/json_patch.go
+++ internal/rules/provider/kubernetes/api/v1alpha2/json_patch.go
@@ -7,19 +7,25 @@ import (
 )
 
 type jsonPatch struct {
-	patchType types.PatchType
-	from      object
-	to        object
+	patchType            types.PatchType
+	from                 object
+	to                   object
+	enableOptimisticLock bool
 }
 
 func (p *jsonPatch) Type() types.PatchType { return p.patchType }
 
 func (p *jsonPatch) Data() ([]byte, error) {
-	original := p.from.DeepCopyObject().(object) // nolint: forcetypeassert
-	modified := p.to.DeepCopyObject().(object)   // nolint: forcetypeassert
+	original := p.from
+	modified := p.to
 
-	modified.SetResourceVersion(original.GetResourceVersion())
-	original.SetResourceVersion("")
+	if p.enableOptimisticLock {
+		original = p.from.DeepCopyObject().(object) // nolint: forcetypeassert
+		modified = p.to.DeepCopyObject().(object)   // nolint: forcetypeassert
+
+		modified.SetResourceVersion(original.GetResourceVersion())
+		original.SetResourceVersion("")
+	}
 
 	patch, err := jsondiff.Compare(original, modified,
 		jsondiff.MarshalFunc(json.Marshal),
@@ -35,6 +41,6 @@ func (p *jsonPatch) Data() ([]byte, error) {
 func (p *jsonPatch) ResourceName() string      { return p.from.GetName() }
 func (p *jsonPatch) ResourceNamespace() string { return p.from.GetNamespace() }
 
-func NewJSONPatch(from, to object) Patch {
-	return &jsonPatch{patchType: types.JSONPatchType, from: from, to: to}
+func NewJSONPatch(from, to object, withOptimisticLock bool) Patch {
+	return &jsonPatch{patchType: types.JSONPatchType, from: from, to: to, enableOptimisticLock: withOptimisticLock}
 }
