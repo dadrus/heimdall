f35f0440c48700f2c3cf269ffd6ee6a26ec16d2f
Dimitrij Drus <dadrus@gmx.de>
2024-03-11 20:08:00 +0100

    more tests

--- internal/x/tlsx/key_store_test.go
+++ internal/x/tlsx/key_store_test.go
@@ -17,11 +17,24 @@
 package tlsx
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"errors"
+	"math/big"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/x/pkix/pemx"
+	"github.com/dadrus/heimdall/internal/x/testsupport"
 )
 
 func TestKeyStoreCertificate(t *testing.T) {
@@ -74,4 +87,83 @@ func TestKeyStoreCertificate(t *testing.T) {
 
 func TestKeyStoreOnChanged(t *testing.T) {
 	t.Parallel()
+
+	// GIVEN
+	testDir := t.TempDir()
+
+	privKey1, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	require.NoError(t, err)
+
+	privKey2, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	require.NoError(t, err)
+
+	cert1, err := testsupport.NewCertificateBuilder(testsupport.WithValidity(time.Now(), 10*time.Hour),
+		testsupport.WithSerialNumber(big.NewInt(1)),
+		testsupport.WithSubject(pkix.Name{
+			CommonName:   "test cert 1",
+			Organization: []string{"Test"},
+			Country:      []string{"EU"},
+		}),
+		testsupport.WithSubjectPubKey(&privKey1.PublicKey, x509.ECDSAWithSHA384),
+		testsupport.WithSelfSigned(),
+		testsupport.WithSignaturePrivKey(privKey1)).
+		Build()
+	require.NoError(t, err)
+
+	cert2, err := testsupport.NewCertificateBuilder(testsupport.WithValidity(time.Now(), 10*time.Hour),
+		testsupport.WithSerialNumber(big.NewInt(1)),
+		testsupport.WithSubject(pkix.Name{
+			CommonName:   "test cert 1",
+			Organization: []string{"Test"},
+			Country:      []string{"EU"},
+		}),
+		testsupport.WithSubjectPubKey(&privKey2.PublicKey, x509.ECDSAWithSHA384),
+		testsupport.WithSelfSigned(),
+		testsupport.WithSignaturePrivKey(privKey2)).
+		Build()
+	require.NoError(t, err)
+
+	pemBytes1, err := pemx.BuildPEM(
+		pemx.WithECDSAPrivateKey(privKey1, pemx.WithHeader("X-Key-ID", "key1")),
+		pemx.WithX509Certificate(cert1),
+	)
+	require.NoError(t, err)
+
+	pemBytes2, err := pemx.BuildPEM(
+		pemx.WithECDSAPrivateKey(privKey2, pemx.WithHeader("X-Key-ID", "key1")),
+		pemx.WithX509Certificate(cert2),
+	)
+	require.NoError(t, err)
+
+	pemFile, err := os.Create(filepath.Join(testDir, "keystore.pem"))
+	require.NoError(t, err)
+
+	_, err = pemFile.Write(pemBytes1)
+	require.NoError(t, err)
+
+	ks := &keyStore{path: pemFile.Name(), keyID: "key1"}
+	ks.load()
+
+	require.Equal(t, cert1, ks.tlsCert.Leaf)
+
+	// WHEN
+	_, err = pemFile.Seek(0, 0)
+	require.NoError(t, err)
+
+	_, err = pemFile.Write(pemBytes2)
+	require.NoError(t, err)
+
+	ks.OnChanged(log.Logger)
+
+	// THEN
+	require.Equal(t, cert2, ks.tlsCert.Leaf)
+
+	// WHEN
+	err = os.Truncate(pemFile.Name(), 0)
+	require.NoError(t, err)
+
+	ks.OnChanged(log.Logger)
+
+	// THEN
+	require.Equal(t, cert2, ks.tlsCert.Leaf)
 }
