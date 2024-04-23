99d5e47af202ee1b6a5f34f322704a6a55bc9155
Dimitrij Drus <dadrus@gmx.de>
2024-04-23 15:27:41 +0200

    package comment moved

--- internal/x/radixtree/tree.go
+++ internal/x/radixtree/tree.go
@@ -1,9 +1,3 @@
-/*
-Package radixtree implements a tree lookup for values associated to
-paths.
-
-This package is a fork of https://github.com/dimfeld/httptreemux.
-*/
 package radixtree
 
 import (
