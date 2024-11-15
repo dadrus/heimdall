2ac78b573bb983b9d516fb504f8d76922e1dca1c
Dimitrij Drus <dadrus@gmx.de>
2024-11-15 22:15:37 +0100

    serve command refactored to make it better testable

--- cmd/serve.go
+++ cmd/serve.go
@@ -19,32 +19,45 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/dadrus/heimdall/cmd/flags"
 	"github.com/dadrus/heimdall/cmd/serve"
 )
 
-// nolint: gochecknoglobals
-var serveCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "Starts the heimdall in one of its operations modes (decision or proxy)",
-}
-
 // nolint: gochecknoinits
 func init() {
-	RootCmd.AddCommand(serveCmd)
+	RootCmd.AddCommand(newServeCmd())
+}
 
-	serveCmd.PersistentFlags().StringP("config", "c", "",
+func newServeCmd() *cobra.Command {
+	cms := &cobra.Command{
+		Use:   "serve",
+		Short: "Starts the heimdall in one of its operations modes (decision or proxy)",
+	}
+
+	cms.PersistentFlags().StringP(flags.Config, "c", "",
 		"Path to heimdall's configuration file.\n"+
 			"If not provided, the lookup sequence is:\n  1. $PWD\n  2. $HOME/.config\n  3. /etc/heimdall/")
-	serveCmd.PersistentFlags().String("env-config-prefix", "HEIMDALLCFG_",
+	cms.PersistentFlags().String(flags.EnvironmentConfigPrefix, "HEIMDALLCFG_",
 		"Prefix for the environment variables to consider for\nloading configuration from")
-	serveCmd.PersistentFlags().Bool("insecure", false,
-		"Disables enforcement of all secure configurations entirely")
-	serveCmd.PersistentFlags().Bool("insecure-no-ingress-tls", false,
-		"Disables enforcement of TLS configuration for ingress traffic")
-	serveCmd.PersistentFlags().Bool("insecure-no-egress-tls", false,
-		"Disables enforcement of TLS configuration for egress traffic")
-	serveCmd.PersistentFlags().Bool("insecure-default-rule", false,
-		"Disables enforcement of secure configuration of the default rule")
-	serveCmd.AddCommand(serve.NewProxyCommand())
-	serveCmd.AddCommand(serve.NewDecisionCommand())
+	cms.PersistentFlags().Bool(flags.SkipAllSecurityEnforcement, false,
+		"Disables enforcement of all secure configurations entirely.\n"+
+			"Effectively it enables all the --skip-*-enforcement flags below.")
+	cms.PersistentFlags().Bool(flags.SkipAllTLSEnforcement, false,
+		"Disables enforcement of TLS for every in- and outbound connection.\n"+
+			"Effectively it enables all the --skip-*-tls-enforcement flags.")
+	cms.PersistentFlags().Bool(flags.SkipIngressTLSEnforcement, false,
+		"Disables enforcement of TLS configuration for ingress traffic.")
+	cms.PersistentFlags().Bool(flags.SkipEgressTLSEnforcement, false,
+		"Disables enforcement of TLS configuration for egress traffic.")
+	cms.PersistentFlags().Bool(flags.SkipManagementTLSEnforcement, false,
+		"Disables enforcement of TLS configuration for the management\nservice.")
+	cms.PersistentFlags().Bool(flags.SkipUpstreamTLSEnforcement, false,
+		"Disables enforcement of TLS while proxying the requests to the\nupstream services.")
+	cms.PersistentFlags().Bool(flags.SkipSecureDefaultRuleEnforcement, false,
+		"Disables enforcement of secure configuration of the default\nrule.")
+
+	cms.AddCommand(serve.NewProxyCommand())
+	cms.AddCommand(serve.NewDecisionCommand())
+
+	return cms
 }
