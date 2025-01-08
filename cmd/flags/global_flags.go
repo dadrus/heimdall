8d489c994803fccdf4157bacc1d765f9eedbc236
Dimitrij Drus <dadrus@gmx.de>
2025-01-08 13:17:01 +0100

    forgotten update

--- cmd/flags/global_flags.go
+++ cmd/flags/global_flags.go
@@ -4,10 +4,13 @@ const (
 	Config                  = "config"
 	EnvironmentConfigPrefix = "env-config-prefix"
 
-	SkipAllSecurityEnforcement       = "insecure"
-	SkipSecureDefaultRuleEnforcement = "insecure-skip-secure-default-rule-enforcement"
-	SkipAllTLSEnforcement            = "insecure-skip-all-tls-enforcement"
-	SkipIngressTLSEnforcement        = "insecure-skip-ingress-tls-enforcement"
-	SkipEgressTLSEnforcement         = "insecure-skip-egress-tls-enforcement"
-	SkipUpstreamTLSEnforcement       = "insecure-skip-upstream-tls-enforcement"
+	SkipAllSecurityEnforcement          = "insecure"
+	SkipSecureTrustedProxiesEnforcement = "insecure-skip-secure-trusted-proxies-enforcement"
+	SkipSecureDefaultRuleEnforcement    = "insecure-skip-secure-default-rule-enforcement"
+	SkipAllTLSEnforcement               = "insecure-skip-all-tls-enforcement"
+	SkipIngressTLSEnforcement           = "insecure-skip-ingress-tls-enforcement"
+	SkipEgressTLSEnforcement            = "insecure-skip-egress-tls-enforcement"
+	SkipUpstreamTLSEnforcement          = "insecure-skip-upstream-tls-enforcement"
+
+	ValidationInProxyMode = "proxy-mode"
 )
