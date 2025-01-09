650567278caebbe0880691cdd1f5b775fe242a4d
Dimitrij Drus <dadrus@gmx.de>
2025-01-09 15:05:59 +0100

    env variables in cmd tests updated

--- cmd/serve/proxy_test.go
+++ cmd/serve/proxy_test.go
@@ -34,8 +34,8 @@ func TestCreateProxyApp(t *testing.T) {
 	port2, err := testsupport.GetFreePort()
 	require.NoError(t, err)
 
-	t.Setenv("SERVE_PROXY_PORT", strconv.Itoa(port1))
-	t.Setenv("SERVE_MANAGEMENT_PORT", strconv.Itoa(port2))
+	t.Setenv("SERVE_PORT", strconv.Itoa(port1))
+	t.Setenv("MANAGEMENT_PORT", strconv.Itoa(port2))
 
 	_, err = createProxyApp(NewProxyCommand())
 	require.NoError(t, err)
