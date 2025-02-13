f5e5971b9aa5c37fb8a9b411dfef03258464c8a0
Dimitrij Drus <dadrus@gmx.de>
2025-02-13 13:06:47 +0100

    chore: Forgotten license info header added

--- internal/x/radixtree/package.go
+++ internal/x/radixtree/package.go
@@ -1,7 +1,8 @@
-/*
-Package radixtree implements a tree lookup for values associated to
-paths.
+// Copyright (c) 2014,2015 Daniel Imfeld
+// Copyright 2022-2025 Dimitrij Drus <dadrus@gmx.de>
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// This package is a modified fork of https://github.com/dimfeld/httptreemux
 
-This package is a fork of https://github.com/dimfeld/httptreemux.
-*/
 package radixtree
