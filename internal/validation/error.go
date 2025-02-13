c4d79df6bfe0bdb6540b963f95f36c8bef67035f
Dimitrij Drus <dadrus@gmx.de>
2025-02-13 14:32:59 +0100

    license header added

--- internal/validation/error.go
+++ internal/validation/error.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Dimitrij Drus <dadrus@gmx.de>
+// Copyright 2022-2025 Dimitrij Drus <dadrus@gmx.de>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
