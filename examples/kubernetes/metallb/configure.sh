862899ec749246a9152e5915ba18dd2d54ebfced
Dimitrij Drus <dadrus@gmx.de>
2025-02-13 16:00:25 +0100

    fixing IPAM ref

--- examples/kubernetes/metallb/configure.sh
+++ examples/kubernetes/metallb/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KIND_SUBNET=$(docker network inspect kind -f "{{(index .IPAM.Config 1).Subnet}}")
+KIND_SUBNET=$(docker network inspect kind -f "{{(index .IPAM.Config 0).Subnet}}")
 METALLB_IP_START=$(echo ${KIND_SUBNET} | sed "s@0.0/16@255.200@")
 METALLB_IP_END=$(echo ${KIND_SUBNET} | sed "s@0.0/16@255.250@")
 METALLB_IP_RANGE="${METALLB_IP_START}-${METALLB_IP_END}"
