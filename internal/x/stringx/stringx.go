8c0d70caa92e992b3f15cf9ee0d3f29a051dd72f
Dimitrij Drus <dadrus@gmx.de>
2025-07-25 01:46:49 +0200

    some code formatting

--- internal/x/stringx/stringx.go
+++ internal/x/stringx/stringx.go
@@ -18,9 +18,7 @@ package stringx
 
 import "unsafe"
 
-func ToString(b []byte) string {
-	return unsafe.String(unsafe.SliceData(b), len(b))
-}
+func ToString(b []byte) string { return unsafe.String(unsafe.SliceData(b), len(b)) }
 
 func ToBytes(str string) []byte {
 	return unsafe.Slice(unsafe.StringData(str), len(str))
