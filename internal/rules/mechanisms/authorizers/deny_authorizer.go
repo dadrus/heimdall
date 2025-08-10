5cf57011e582fbf21e0f335dd7c03172bb38c1d7
Dimitrij Drus <dadrus@gmx.de>
2025-08-10 18:04:16 +0200

    deny authorizer updated to support step ids

--- internal/rules/mechanisms/authorizers/deny_authorizer.go
+++ internal/rules/mechanisms/authorizers/deny_authorizer.go
@@ -30,38 +30,64 @@ import (
 //nolint:gochecknoinits
 func init() {
 	registerTypeFactory(
-		func(app app.Context, id string, typ string, _ map[string]any) (bool, Authorizer, error) {
+		func(app app.Context, name string, typ string, _ map[string]any) (bool, Authorizer, error) {
 			if typ != AuthorizerDeny {
 				return false, nil, nil
 			}
 
-			return true, newDenyAuthorizer(app, id), nil
+			return true, newDenyAuthorizer(app, name), nil
 		})
 }
 
 type denyAuthorizer struct {
-	id string
+	name string
+	id   string
 }
 
-func newDenyAuthorizer(app app.Context, id string) *denyAuthorizer {
+func newDenyAuthorizer(app app.Context, name string) *denyAuthorizer {
 	logger := app.Logger()
-	logger.Info().Str("_id", id).Msg("Creating deny authorizer")
+	logger.Info().
+		Str("_type", AuthorizerDeny).
+		Str("_name", name).
+		Msg("Creating authorizer")
 
-	return &denyAuthorizer{id: id}
+	return &denyAuthorizer{
+		name: name,
+		id:   name,
+	}
 }
 
 func (a *denyAuthorizer) Execute(ctx heimdall.RequestContext, _ *subject.Subject) error {
 	logger := zerolog.Ctx(ctx.Context())
-	logger.Debug().Str("_id", a.id).Msg("Authorizing using deny authorizer")
+	logger.Debug().
+		Str("_type", AuthorizerDeny).
+		Str("_name", a.name).
+		Str("_id", a.id).
+		Msg("Executing authorizer")
 
 	return errorchain.
 		NewWithMessage(heimdall.ErrAuthorization, "denied by authorizer").
 		WithErrorContext(a)
 }
 
-func (a *denyAuthorizer) WithConfig(stepID string, _ map[string]any) (Authorizer, error) {
-	return a, nil
+func (a *denyAuthorizer) WithConfig(stepID string, rawConfig map[string]any) (Authorizer, error) {
+	if len(stepID) == 0 && len(rawConfig) == 0 {
+		return a, nil
+	}
+
+	if len(rawConfig) != 0 {
+		return nil, errorchain.
+			NewWithMessage(heimdall.ErrConfiguration, "allow authorizer cannot be reconfigured").
+			WithErrorContext(a)
+	}
+
+	auth := *a
+	auth.id = stepID
+
+	return &auth, nil
 }
+
+func (a *denyAuthorizer) Name() string { return a.name }
 
 func (a *denyAuthorizer) ID() string { return a.id }
 
