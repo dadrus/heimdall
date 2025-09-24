e89ebb3c89cef2156f4d1114bba1da830b7900a2
Dimitrij Drus <dadrus@gmx.de>
2025-09-24 23:58:36 +0200

    further updates

--- internal/rules/provider/kubernetes/webhooks/conversion/types.go
+++ internal/rules/provider/kubernetes/webhooks/conversion/types.go
@@ -1,8 +1,23 @@
+// Copyright 2025 Dimitrij Drus <dadrus@gmx.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package conversion
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/goccy/go-json"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -30,21 +45,32 @@ type (
 	review struct{}
 )
 
+// nolint: gochecknoglobals
+var knownReasons = map[int32]metav1.StatusReason{
+	http.StatusUnauthorized:          metav1.StatusReasonUnauthorized,
+	http.StatusForbidden:             metav1.StatusReasonForbidden,
+	http.StatusNotFound:              metav1.StatusReasonNotFound,
+	http.StatusConflict:              metav1.StatusReasonConflict,
+	http.StatusGone:                  metav1.StatusReasonGone,
+	http.StatusUnprocessableEntity:   metav1.StatusReasonInvalid,
+	http.StatusGatewayTimeout:        metav1.StatusReasonServerTimeout,
+	http.StatusRequestTimeout:        metav1.StatusReasonTimeout,
+	http.StatusTooManyRequests:       metav1.StatusReasonTooManyRequests,
+	http.StatusBadRequest:            metav1.StatusReasonBadRequest,
+	http.StatusMethodNotAllowed:      metav1.StatusReasonMethodNotAllowed,
+	http.StatusNotAcceptable:         metav1.StatusReasonNotAcceptable,
+	http.StatusRequestEntityTooLarge: metav1.StatusReasonRequestEntityTooLarge,
+	http.StatusUnsupportedMediaType:  metav1.StatusReasonUnsupportedMediaType,
+	http.StatusInternalServerError:   metav1.StatusReasonInternalError,
+	http.StatusServiceUnavailable:    metav1.StatusReasonServiceUnavailable,
+}
+
 func (r *request) GetUID() string { return string(r.UID) }
 
-func withReasons(reasons ...string) responseOption {
+func withErrorDetails(details ...metav1.StatusCause) responseOption {
 	return func(resp *response) {
-		if len(reasons) > 0 {
-			resp.Result.Details = &metav1.StatusDetails{Causes: make([]metav1.StatusCause, len(reasons))}
-
-			for idx, reason := range reasons {
-				resp.Result.Details.Causes[idx] = metav1.StatusCause{Message: reason}
-			}
-
-			// Unfortunately details alone are not sufficient. At least when using kubectl
-			// if no Reason is set, only the Message (see above) is printed, which
-			// typically does not provide any details which could help resolving the issue
-			resp.Result.Reason = metav1.StatusReason(strings.Join(reasons, "; "))
+		if len(details) > 0 {
+			resp.Result.Details = &metav1.StatusDetails{Causes: details}
 		}
 	}
 }
@@ -66,6 +92,10 @@ func newResponse(code int, msg string, opts ...responseOption) *response {
 			Status:  x.IfThenElse(code == http.StatusOK, metav1.StatusSuccess, metav1.StatusFailure),
 			Message: msg,
 		},
+	}
+
+	if resp.Result.Status != metav1.StatusSuccess {
+		resp.Result.Reason = knownReasons[resp.Result.Code]
 	}
 
 	for _, opt := range opts {
