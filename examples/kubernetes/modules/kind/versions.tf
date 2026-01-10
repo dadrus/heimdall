e63d2452d70af31a56d1a3b637a436fd16841835
Dimitrij Drus <dadrus@gmx.de>
2026-01-10 12:32:31 +0100

    chore: Kind and MetalLB versions updated

--- examples/kubernetes/modules/kind/versions.tf
+++ examples/kubernetes/modules/kind/versions.tf
@@ -1,6 +1,6 @@
 variable "node_image" {
   type        = string
-  default     = "kindest/node:v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027"
+  default     = "kindest/node:v1.35.0@sha256:4613778f3cfcd10e615029370f5786704559103cf27bef934597ba562b269661"
   description = "Node image for kind nodes"
 }
 
@@ -12,6 +12,6 @@ variable "image_registry_proxy" {
 
 variable "metallb_version" {
   type        = string
-  default     = "0.15.2"
+  default     = "0.15.3"
   description = "Helm chart version for MetalLB"
 }
