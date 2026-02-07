3dceeeddadb111455ab0fa7ab0e5be0eea8f49f6
Dimitrij Drus <dadrus@gmx.de>
2026-02-07 20:55:16 +0100

    decoder for the new custom type - enbles decoding references from a simple string, as well as from structured definitions

---
+++ internal/rules/mechanisms/authenticators/mapstructure_decoder_test.go
@@ -0,0 +1,101 @@
+// Copyright 2022 Dimitrij Drus <dadrus@gmx.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package authenticators
+
+import (
+	"testing"
+
+	"github.com/go-viper/mapstructure/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dadrus/heimdall/internal/x/testsupport"
+)
+
+func TestDecodeAttributeRefsFunc(t *testing.T) {
+	t.Parallel()
+
+	// GIVEN
+	for uc, tc := range map[string]struct {
+		config []byte
+		assert func(t *testing.T, err error, pi PrincipalInfo)
+	}{
+		"can decode attribute references from just a string": {
+			config: []byte(`
+id: foo
+attributes: bar
+`),
+			assert: func(t *testing.T, err error, pi PrincipalInfo) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, "foo", pi.IDFrom)
+				assert.Len(t, pi.AttributesFrom, 1)
+				assert.Equal(t, "bar", pi.AttributesFrom[""])
+			},
+		},
+		"can decode attributes from structured definition": {
+			config: []byte(`
+id: foo
+attributes:
+  a: bar
+  b: baz
+`),
+			assert: func(t *testing.T, err error, pi PrincipalInfo) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, "foo", pi.IDFrom)
+				assert.Len(t, pi.AttributesFrom, 2)
+				assert.Equal(t, "bar", pi.AttributesFrom["a"])
+				assert.Equal(t, "baz", pi.AttributesFrom["b"])
+			},
+		},
+		"can decode if attributes are not specified": {
+			config: []byte(`id: foo`),
+			assert: func(t *testing.T, err error, pi PrincipalInfo) {
+				t.Helper()
+
+				require.NoError(t, err)
+				assert.Equal(t, "foo", pi.IDFrom)
+				assert.Empty(t, pi.AttributesFrom)
+			},
+		},
+	} {
+		t.Run(uc, func(t *testing.T) {
+			// GIVEN
+			conf, err := testsupport.DecodeTestConfig(tc.config)
+			require.NoError(t, err)
+
+			var pi PrincipalInfo
+
+			dec, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+				DecodeHook: mapstructure.ComposeDecodeHookFunc(
+					DecodeAttributeRefs(),
+				),
+				Result: &pi,
+			})
+			require.NoError(t, err)
+
+			// WHEN
+			err = dec.Decode(conf)
+
+			// THEN
+			tc.assert(t, err, pi)
+		})
+	}
+}
