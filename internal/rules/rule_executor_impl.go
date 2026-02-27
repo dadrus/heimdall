1b5a2455752675ef8485011acfb26e89cf80396b
Dimitrij Drus <dadrus@gmx.de>
2026-02-27 10:15:35 +0100

    obsolete code removed

--- internal/rules/rule_executor_impl.go
+++ internal/rules/rule_executor_impl.go
@@ -17,41 +17,24 @@
 package rules
 
 import (
-	"time"
-
 	"github.com/rs/zerolog"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/trace"
 
-	"github.com/dadrus/heimdall/internal/otel/semconv"
 	"github.com/dadrus/heimdall/internal/pipeline"
 	"github.com/dadrus/heimdall/internal/rules/rule"
 )
 
 type ruleExecutor struct {
-	r  rule.Repository
-	t  trace.Tracer
-	rd semconv.RuleExecutionDuration
+	r rule.Repository
 }
 
-func newRuleExecutor(repository rule.Repository, meter metric.Meter, tracer trace.Tracer) pipeline.Executor {
-	rd, _ := semconv.NewRuleExecutionDuration(meter)
-
-	return &ruleExecutor{
-		r:  repository,
-		t:  tracer,
-		rd: rd,
-	}
+func newRuleExecutor(repository rule.Repository) pipeline.Executor {
+	return &ruleExecutor{r: repository}
 }
 
 func (e *ruleExecutor) Execute(hctx pipeline.Context) (pipeline.Backend, error) {
-	startTime := time.Now()
 	request := hctx.Request()
-	ctx := hctx.Context()
 
-	zerolog.Ctx(ctx).Debug().
+	zerolog.Ctx(hctx.Context()).Debug().
 		Str("_method", request.Method).
 		Str("_url", request.URL.String()).
 		Msg("Analyzing request")
@@ -61,30 +44,5 @@ func (e *ruleExecutor) Execute(hctx pipeline.Context) (pipeline.Backend, error) 
 		return nil, err
 	}
 
-	ctx, span := e.t.Start(
-		ctx,
-		"Rule Execution",
-		trace.WithSpanKind(trace.SpanKindInternal),
-		trace.WithAttributes(
-			semconv.RuleIDKey.String(rul.ID()),
-			semconv.RuleSetKey.String(rul.SrcID()),
-		),
-	)
-
-	defer span.End()
-
-	be, err := rul.Execute(hctx.WithParent(ctx))
-	elapsedTime := float64(time.Since(startTime)) / float64(time.Millisecond)
-
-	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, err.Error())
-	}
-
-	e.rd.Record(ctx, elapsedTime, attribute.NewSet(
-		e.rd.AttrRuleID(rul.ID()),
-		e.rd.AttrRuleSet(rul.SrcID()),
-	))
-
-	return be, err
+	return rul.Execute(hctx)
 }
