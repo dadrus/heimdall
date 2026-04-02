15646b0b00e2582cdcba992846f686c4418db8d1
Dimitrij Drus <dadrus@gmx.de>
2026-04-02 19:50:10 +0200

    collecting cache usage metrics only if enabled

--- internal/cache/registry/registry.go
+++ internal/cache/registry/registry.go
@@ -61,5 +61,9 @@ func Create(ctx app.Context, typ string, config map[string]any) (types.Cache, er
 		return nil, err
 	}
 
-	return metrics.Decorate(ctx, cch)
+	if ctx.Config().Metrics.CoverCache {
+		return metrics.Decorate(ctx, cch)
+	}
+
+	return cch, nil
 }
