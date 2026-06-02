52597029681c66888ddb881beb8264de21a05f6f
Dimitrij Drus <dadrus@gmx.de>
2026-06-02 09:20:22 +0200

    license headers added

--- internal/x/fswatch/event_handler.go
+++ internal/x/fswatch/event_handler.go
@@ -1,3 +1,19 @@
+// Copyright 2026 Dimitrij Drus
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package fswatch
 
 // Op describes the type of change observed for a path.
@@ -6,14 +22,13 @@ type Op uint8
 const (
 	// OpAdded means that a path was added below a watched directory.
 	//
-	// OpAdded is emitted only for entries below watched directories. It is not
-	// emitted during initial watcher setup.
+	// OpAdded is emitted only for entries below watched directories.
 	OpAdded Op = iota + 1
 
 	// OpChanged means that the content or target of a watched path changed.
 	//
-	// For file roots, this means the watched file changed.
-	// For directory roots, this can either mean a child changed or that the
+	// For files, this means the watched file changed.
+	// For directories, this can either mean a child changed or that the
 	// watched directory itself was rebound, e.g. by a symlink target switch.
 	OpChanged
 
@@ -53,7 +68,6 @@ type EventHandler interface {
 // EventHandlerFunc adapts a function to EventHandler.
 type EventHandlerFunc func(evt Event) error
 
-// HandleEvent implements EventHandler.
 func (f EventHandlerFunc) HandleEvent(evt Event) error {
 	return f(evt)
 }
