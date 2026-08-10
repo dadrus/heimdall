9f3181bb15edf633bc3e3e0e6dfee8b007eec1f0
Dimitrij Drus <dadrus@gmx.de>
2026-08-10 20:59:03 +0200

    http trusted proxy middleware updated

--- internal/handler/middleware/http/trustedproxy/handler.go
+++ internal/handler/middleware/http/trustedproxy/handler.go
@@ -19,78 +19,22 @@ package trustedproxy
 import (
 	"net"
 	"net/http"
-	"slices"
-	"strings"
 
 	"github.com/rs/zerolog"
 
-	"github.com/dadrus/heimdall/internal/config"
+	"github.com/dadrus/heimdall/internal/handler/middleware/trustedproxy"
 	"github.com/dadrus/heimdall/internal/x/httpx"
 )
 
-var untrustedHeader = []string{ //nolint:gochecknoglobals
-	"Forwarded",
-	"X-Forwarded-For",
-	"X-Forwarded-Proto",
-	"X-Forwarded-Host",
-	"X-Forwarded-Uri",
-	"X-Forwarded-Path",
-	"X-Forwarded-Method",
-}
-
-type ipHolder interface {
-	Contains(ip net.IP) bool
-}
-
-type simpleIP net.IP
-
-func (s simpleIP) Contains(ip net.IP) bool {
-	return net.IP(s).Equal(ip)
-}
-
-type trustedProxySet []ipHolder
-
-func (tpm trustedProxySet) Contains(ip net.IP) bool {
-	for _, proxy := range tpm {
-		if proxy.Contains(ip) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func New(logger zerolog.Logger, proxies ...string) func(http.Handler) http.Handler {
-	var ipHolders []ipHolder
-
-	for _, ipAddr := range proxies {
-		if !strings.Contains(ipAddr, "/") {
-			ipHolders = append(ipHolders, simpleIP(net.ParseIP(ipAddr)))
-
-			continue
-		}
-
-		_, ipNet, err := net.ParseCIDR(ipAddr)
-		if err != nil {
-			logger.Warn().Err(err).
-				Msgf("Trusted proxies entry %q could not be parsed and will be ignored", ipAddr)
-		} else {
-			ipHolders = append(ipHolders, ipNet)
-
-			if slices.Contains(config.InsecureNetworks, ipNet.String()) {
-				logger.Warn().Msgf("Configured trusted proxies contains insecure networks: %s", ipAddr)
-			}
-		}
-	}
-
-	trustedProxies := trustedProxySet(ipHolders)
+	matcher := trustedproxy.NewMatcher(logger, proxies...)
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			if !trustedProxies.Contains(net.ParseIP(httpx.IPFromHostPort(req.RemoteAddr))) {
-				for _, name := range untrustedHeader {
-					req.Header.Del(name)
-				}
+			remoteIP := net.ParseIP(httpx.IPFromHostPort(req.RemoteAddr))
+
+			if !matcher.Contains(remoteIP) {
+				trustedproxy.RemoveForwardingHeaders(req.Header.Del)
 			}
 
 			next.ServeHTTP(rw, req)
