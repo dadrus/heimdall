0ef1baffc42360e4ba363fe68e8c66a796afcc35
Dimitrij Drus <dadrus@gmx.de>
2026-08-14 17:11:14 +0200

    fix: Ensuring generic authenticator does not follow redirects (#3468)

--- internal/rules/mechanisms/authenticators/generic_authenticator.go
+++ internal/rules/mechanisms/authenticators/generic_authenticator.go
@@ -262,7 +262,12 @@ func (a *genericAuthenticator) getSubjectInformation(ctx heimdall.RequestContext
 }
 
 func (a *genericAuthenticator) fetchSubjectInformation(req *http.Request) ([]byte, error) {
-	resp, err := a.e.CreateClient(req.URL.Hostname()).Do(req)
+	client := a.e.CreateClient(req.URL.Hostname())
+	client.CheckRedirect = func(_ *http.Request, _ []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		var clientErr *url.Error
 		if errors.As(err, &clientErr) && clientErr.Timeout() {
