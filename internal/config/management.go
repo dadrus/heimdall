9b732f4841dcb206d1bfe17266208be883b2576b
Dimitrij Drus <dadrus@gmx.de>
2026-09-06 22:20:30 +0200

    management updated as well

--- internal/config/management.go
+++ internal/config/management.go
@@ -19,12 +19,13 @@ package config
 import "fmt"
 
 type ManagementConfig struct {
-	Host        string      `koanf:"host"`
-	Port        int         `koanf:"port"`
-	Timeout     Timeout     `koanf:"timeout"`
-	BufferLimit BufferLimit `koanf:"buffer_limit"`
-	CORS        *CORS       `koanf:"cors,omitempty"`
-	TLS         *TLS        `koanf:"tls,omitempty"  validate:"enforced=notnil"`
+	Host        string             `koanf:"host"`
+	Port        int                `koanf:"port"`
+	Requests    IngressRequests    `koanf:"requests"`
+	Responses   IngressResponses   `koanf:"responses"`
+	Connections IngressConnections `koanf:"connections"`
+	CORS        *CORS              `koanf:"cors,omitempty"`
+	TLS         *TLS               `koanf:"tls,omitempty"  validate:"enforced=notnil"`
 }
 
 func (c ManagementConfig) Address() string { return fmt.Sprintf("%s:%d", c.Host, c.Port) }
