8e165cacd3fd4d334928006b17c4de799e7e3044
Dimitrij Drus <dadrus@gmx.de>
2026-09-06 22:19:35 +0200

    useless mapstructure tag removed

--- internal/config/secret.go
+++ internal/config/secret.go
@@ -17,6 +17,6 @@
 package config
 
 type Secret struct {
-	Source   string `koanf:"source"   mapstructure:"source"   validate:"required"`
-	Selector string `koanf:"selector" mapstructure:"selector"`
+	Source   string `koanf:"source" validate:"required"`
+	Selector string `koanf:"selector"`
 }
